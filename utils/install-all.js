d5a49e1d7f8f0f704e9adc99c742abb3d70a7d6e
Ketan-Khalate <ketan.khalate@waiin.com>
2024-01-18 22:22:57 +0530

    committed changes for making required changes in install-all.js file

--- utils/install-all.js
+++ utils/install-all.js
@@ -1,17 +1,10 @@
 // Imports
 let path = require("path");
 let { execSync } = require("child_process");
-const coreFolderPath = path.join(__dirname, "..", "raaghu-react-core");
 const elementsFolderPath = path.join(__dirname, "..", "raaghu-elements");
 const componentsFolderPath = path.join(__dirname, "..", "raaghu-components");
-const mfeFolderPath = path.join(__dirname, "..", "raaghu-mfe");
 
 console.log("\x1b[32m%s\x1b[0m", `Installing dependencies...`);
-execSync(`npm install`, {
-    cwd: coreFolderPath,
-    stdio: "inherit",
-});
-
 execSync(`npm install`, {
     cwd: elementsFolderPath,
     stdio: "inherit",
@@ -19,11 +12,6 @@ execSync(`npm install`, {
 
 execSync(`npm install`, {
     cwd: componentsFolderPath,
-    stdio: "inherit",
-});
-
-execSync(`npm install`, {
-    cwd: mfeFolderPath,
     stdio: "inherit",
 });
 
