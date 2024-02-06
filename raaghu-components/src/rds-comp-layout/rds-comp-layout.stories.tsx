9681214243483c0f89900f30711a9ebcdba353ce
neeraj-bhakre <neeraj.bhakre@waiin.com>
2024-02-06 17:32:25 +0530

    Commit for layout creation chnages

--- raaghu-components/src/rds-comp-layout/rds-comp-layout.stories.tsx
+++ raaghu-components/src/rds-comp-layout/rds-comp-layout.stories.tsx
@@ -3,10 +3,29 @@ import RdsCompLayout from "./rds-comp-layout";
 
 export default {
   title: "Layout",
+  component: RdsCompLayout,
+  argTypes: {
+    displayType: {
+      control: {
+        type: 'select',
+        options: ['layout1', 'layout2', 'layout3']
+      },
+      defaultValue: 'layout1'
+    }
+  }
 };
 
-export const Default = () => <RdsCompLayout />;
+export const Layout1 = (args) => <RdsCompLayout {...args} />;
+Layout1.args = {
+  displayType: 'layout1',
+};
 
-Default.story = {
-  name: 'default',
+export const Layout2 = (args) => <RdsCompLayout {...args} />;
+Layout2.args = {
+  displayType: 'layout2',
+};
+
+export const Layout3 = (args) => <RdsCompLayout {...args} />;
+Layout3.args = {
+  displayType: 'layout3',
 };
--- raaghu-components/src/rds-comp-layout/rds-comp-layout.tsx
+++ raaghu-components/src/rds-comp-layout/rds-comp-layout.tsx
@@ -1,12 +1,83 @@
 import React, { FC } from 'react';
 import { RdsCompLayoutWrapper } from './rds-comp-layout.styled';
 
-interface RdsCompLayoutProps {}
+interface RdsCompLayoutProps {
+   displayType?: 'layout1' | 'layout2'| 'layout3';
+}
 
-const RdsCompLayout: FC<RdsCompLayoutProps> = () => (
- <RdsCompLayoutWrapper>
-    RdsCompLayout Component
- </RdsCompLayoutWrapper>
+const LayoutComponents: {
+  [key in RdsCompLayoutProps["displayType"]]: React.ReactNode;
+} = {
+  layout1: (
+    <div className="layout1">
+      <header>
+        <h1>Header</h1>
+      </header>
+      <div className="main">
+        <aside className="left">Sidebar</aside>
+        <main className="right">
+        <div className="container">
+          <div className="row bg-white">
+            <div className="content">Content</div>
+          </div>
+        </div>
+        </main>
+      </div>
+    </div>
+  ),
+  layout2: (
+    <div className="layout2">
+      <header>
+        <h1>Header</h1>
+      </header>
+      <div className="main">
+        <aside className="left">Sidebar</aside>
+        <main className="right">
+          <div className="container">
+            <div className="row">
+              <div className="col bg-white">Column 1</div>
+              <div className="col bg-white">Column 2</div>
+              <div className="col bg-white">Column 3</div>
+            </div>
+            <div className="row bg-white">
+              <div className="content">Content</div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  ),
+  layout3: (
+    <div className="layout3">
+      <header>
+        <h1>Header</h1>
+      </header>
+      <div className="main">
+        <aside className="left">Sidebar</aside>
+        <main className="right">
+          <div className="container">
+            <div className="row bg-white">
+              <div className="col bg-white">Column 1</div>
+            </div>
+            <div className="row">
+              <div className="col bg-white">Column 1</div>
+              <div className="col bg-white">Column 2</div>
+              <div className="col bg-white">Column 3</div>
+            </div>
+            <div className="row bg-white">
+              <div className="content">Content</div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  ),
+};
+
+const RdsCompLayout: FC<RdsCompLayoutProps> = ({ displayType = 'layout1' }) => (
+  <RdsCompLayoutWrapper>
+    {LayoutComponents[displayType]}
+  </RdsCompLayoutWrapper>
 );
 
 export default RdsCompLayout;
