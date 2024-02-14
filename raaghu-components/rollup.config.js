f66aff5fbc817c323fb68bed7bd6f8eb5eb6f0d1
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-02-14 10:06:46 +0530

    Integrate the million dev package in storybook

--- raaghu-components/rollup.config.js
+++ raaghu-components/rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import million from "million/compiler";
 
 const packageJson = require("./package.json");
 
@@ -24,6 +25,12 @@ export default [
             },
         ],
         plugins: [
+            million.rollup({
+                auto: {
+                  threshold: 0.05,
+                  skip: ["useBadHook", /badVariable/g],
+                },
+              }),
             resolve({ preferBuiltins: true, mainFields: ["browser"] }),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json", sourceMap: false }),
