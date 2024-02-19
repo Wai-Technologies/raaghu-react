9e37c3b4c52a92bfcd877f439625fcf8f0b4be75
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-02-19 16:59:14 +0530

    Update vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    terserOptions: {
+      mangle: {
+        reserved: ['RdsAvatar'], // Replace 'RdsAvatar' with the actual name of the component you want to preserve during minification
+      },
+    },
+  },
 })
