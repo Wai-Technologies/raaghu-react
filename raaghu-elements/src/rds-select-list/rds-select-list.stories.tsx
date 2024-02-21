896d0c31441596abbc5028ed1516339b1134c256
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-02-21 19:13:13 +0530

    update elements list

--- raaghu-elements/src/rds-select-list/rds-select-list.stories.tsx
+++ raaghu-elements/src/rds-select-list/rds-select-list.stories.tsx
@@ -1,50 +1,56 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 import RdsSelectList from "./rds-select-list";
+import { Meta, StoryObj } from "@storybook/react";
 
-export default {
+
+const meta: Meta = {
     title: "Elements/SelectList",
     component: RdsSelectList,
     argTypes: {
-        size: {
-            options: ["lg",
-                "md",
-                "sm"
-            ],
-            control: { type: "select" },
-        },
+        // size: {
+        //     options: ["lg",
+        //         "md",
+        //         "sm"
+        //     ],
+        //     control: { type: "select" },
+        // },
+    },
+    parameters: {
+        layout: 'padded',
+      },
+      tags: ['autodocs']
+} satisfies Meta<typeof RdsSelectList>;
+
+
+export default meta;
+type Story = StoryObj<typeof RdsSelectList>;
+
+export const Default: Story = { 
+    args : {
+        id:"story",
+        label:"Open select list",
+        isMultiple:false,
+        placeholder:"Select option",
+        selectItems:[
+            {
+                option:"One",
+                value:"one"
+            },
+            {
+                option:"two",
+                value:"two"
+            },
+            {
+                option:"three",
+                value:"three"
+            },
+            {
+                option:"four",
+                value:"four"
+            }
+    
+        ],
+        isDisabled:false,
+        selectedValue:"",
     }
-} as ComponentMeta<typeof RdsSelectList>;
-
-const Template: ComponentStory<typeof RdsSelectList> = (args) => (
-    <RdsSelectList {...args} />
-);
-
-export const SelectList = Template.bind({});
-SelectList.args = {
-    id:"story",
-    label:"Open select list",
-    isMultiple:false,
-    placeholder:"Select option",
-    selectItems:[
-        {
-            option:"One",
-            value:"one"
-        },
-        {
-            option:"two",
-            value:"two"
-        },
-        {
-            option:"three",
-            value:"three"
-        },
-        {
-            option:"four",
-            value:"four"
-        }
-
-    ],
-    isDisabled:false,
-    selectedValue:"",
-};
+}
