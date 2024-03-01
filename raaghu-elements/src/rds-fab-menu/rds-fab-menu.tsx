b1abc9396a22e6f71644a416be6083b38eb94072
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-03-01 15:58:12 +0530

    Formatted and removed extra imports from elements

--- raaghu-elements/src/rds-fab-menu/rds-fab-menu.tsx
+++ raaghu-elements/src/rds-fab-menu/rds-fab-menu.tsx
@@ -4,19 +4,18 @@ import { colors } from "../../libs/types";
 import RdsIcon from "../rds-icon";
 
 export interface RdsFabMenuProps {
-  colorVariant?: colors;
-  size?: string;
-  menuIcon?: string;
-  menuiconWidth?: string;
-  menuiconHeight?: string;
-  listItems: any[];
-  onClick?: ( React.MouseEventHandler<HTMLButtonElement>);
+    colorVariant?: colors;
+    size?: string;
+    menuIcon?: string;
+    menuiconWidth?: string;
+    menuiconHeight?: string;
+    listItems: any[];
+    onClick?: (React.MouseEventHandler<HTMLButtonElement>);
 }
 
 const RdsFabMenu = (props: RdsFabMenuProps) => {
-    const customClasses = `btn btn-${props.colorVariant} btn-icon fab-btn ${
-        props.size == "small" ? "btn-sm" : props.size == "large" ? "btn-lg" : ""
-    }`;
+    const customClasses = `btn btn-${props.colorVariant} btn-icon fab-btn ${props.size == "small" ? "btn-sm" : props.size == "large" ? "btn-lg" : ""
+        }`;
 
     return (
         <>
@@ -37,7 +36,7 @@ const RdsFabMenu = (props: RdsFabMenuProps) => {
                 ></RdsIcon>
             </button>
             <div className="dropdown-menu fab-dropdown border-0 shadow mb-1" role="menu">
-                {props.listItems.map((listItem)=>(
+                {props.listItems.map((listItem) => (
                     <a role="link" className="dropdown-item fab-dropdown-item d-flex py-3" onClick={listItem.onClick}>
                         <RdsIcon name={listItem.icon} height={listItem.iconHeight} width={listItem.iconWidth} fill={false} stroke={true}></RdsIcon>
                         <span className="ms-3">{listItem.value}</span>
--- raaghu-elements/src/rds-feature-list/rds-feature-list.tsx
+++ raaghu-elements/src/rds-feature-list/rds-feature-list.tsx
@@ -1,11 +1,11 @@
-import React, { Fragment, ReactNode } from "react";
+import React, { Fragment } from "react";
 import "./rds-feature-list.css";
 
 export interface RdsFeatureListProps {
     heading: string
     itemList: string[]
     columns: number,
-    fontStyle?:"italic" |"normal";
+    fontStyle?: "italic" | "normal";
     colorVariant?: "primary" | "success" | "danger" | "warning" | "light" | "info" | "secondary" | "dark";
 }
 
@@ -14,8 +14,8 @@ const RdsFeatureList = (props: RdsFeatureListProps) => {
     const columnArray = (props.columns > 0) ? Array.from(Array(Math.min(props.columns, props.itemList.length)), (_: any, index: number) => index + 1) : [1];
     const listItemInEachRow = props.itemList.length / noOfColumns < 1 ? 1 : Math.ceil(props.itemList.length / noOfColumns);
     const ExtraListElements = props.itemList.length % noOfColumns;
-    const fontStyle = props.fontStyle =="italic"?" fst-italic fw-normal ":" fw-normal "; 
-    const color = " text-"+props.colorVariant||"dark" ;
+    const fontStyle = props.fontStyle == "italic" ? " fst-italic fw-normal " : " fw-normal ";
+    const color = " text-" + props.colorVariant || "dark";
     return (
         <>
             <div>
@@ -26,8 +26,8 @@ const RdsFeatureList = (props: RdsFeatureListProps) => {
                             <ul className="RdsFeatureList__Ul">
                                 {props.itemList.map((item, index) =>
                                     <Fragment key={"listitem" + index + "-col" + colNumber}>
-                                        {index >= ((colNumber-1) * listItemInEachRow  -  ((colNumber-ExtraListElements>0&&ExtraListElements>0)?colNumber-ExtraListElements-1 :0 ) )
-                                            && index < ((colNumber) * listItemInEachRow  -  ((colNumber-ExtraListElements>0&&ExtraListElements>0)?colNumber-ExtraListElements :0 ) )
+                                        {index >= ((colNumber - 1) * listItemInEachRow - ((colNumber - ExtraListElements > 0 && ExtraListElements > 0) ? colNumber - ExtraListElements - 1 : 0))
+                                            && index < ((colNumber) * listItemInEachRow - ((colNumber - ExtraListElements > 0 && ExtraListElements > 0) ? colNumber - ExtraListElements : 0))
                                             && <li>
                                                 <span className="bullet">&bull;</span>
                                                 <span className={fontStyle} >{item}</span>
