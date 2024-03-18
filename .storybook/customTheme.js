92d795d6267a43cf652c7b2a236e4fb1222a92da
neeraj-bhakre <neeraj.bhakre@waiin.com>
2024-03-18 15:05:10 +0530

    Committed for chnage brand logo icon

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -1,36 +1,9 @@
 import { create } from "@storybook/theming";
-import logoUrl from "./assets/raaghu-logo.svg";
-// .storybook/preview.js
-
-import './custom-theme.css'; // Your local styles
-import 'bootstrap/dist/css/bootstrap.min.css'; // Styles from node_modules
+import './custom-theme.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default create({
 	base: "light",
-	brandTitle: "Raaghu",
+	brandTitle: "Raaghu Design System",
 	brandUrl: "https://react.raaghu.io",
-	brandImage: logoUrl,
-	// colorPrimary: '#3A10E5',
-	//colorSecondary: '#585C6D',
-
-	// UI
-	// appBg: '#ffffff',
-	// appContentBg: '#ffffff',
-	// appBorderColor: '#585C6D',
-	// appBorderRadius: 4,
-
-	// Text colors
-	// textColor: '#10162F',
-	// textInverseColor: '#ffffff',
-
-	// Toolbar default and active colors
-	// barTextColor: '#9E9E9E',
-	//barSelectedTextColor:'#AC31E1',
-	// barBg: '#ffffff',
-
-	// Form colors
-	// inputBg: '#ffffff',
-	// inputBorder: '#CED4DA',
-	// inputTextColor: '#10162F',
-	// inputBorderRadius: 4,
-
+	brandImage: "https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/raaghu-design-system-lightmode.png"
 });
