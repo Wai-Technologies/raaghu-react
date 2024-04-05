a8b4b85004073df7934bfec2f478e461abebe3bc
AkshayPotphode <155605401+AkshayPotphode@users.noreply.github.com>
2024-04-05 16:38:03 +0530

    Enhance Billing Address and Change User Password Components

--- raaghu-components/src/rds-comp-change-user-password/rds-comp-change-user-password.tsx
+++ raaghu-components/src/rds-comp-change-user-password/rds-comp-change-user-password.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { RdsInput, RdsButton } from "../rds-elements";
+import { set } from "date-fns";
+
 export interface RdsChangePasswordProps {
     changePasswordData?: any;
-    PasswordDataSubmit?: any;
+    onSaveHandler?: (data: any) => void;
 }
 
 const RdsCompChangeUserPassword = (props: RdsChangePasswordProps) => {
@@ -15,97 +17,98 @@ const RdsCompChangeUserPassword = (props: RdsChangePasswordProps) => {
         setChangePassword(props.changePasswordData);
     }, [props.changePasswordData]);
 
-    function setCurrentPassword(value: any) {
-        setChangePassword({ ...changePassword, currentPassword: value });
-    }
-
-    function setNewPassword(value: any) {
-        setChangePassword({ ...changePassword, newPassword: value });
-    }
-
-    function setConfirmNewPassword(value: any) {
-        setChangePassword({ ...changePassword, newPasswordConfirm: value });
-        if (value !== changePassword?.newPassword) {
-            setErrorMessage("Confirm password should be same as new password");
+    const handleDataChanges = (value: any, key: string) => {
+        setChangePassword({ ...changePassword, [key]: value });
+        if (key === 'newPasswordConfirm' && value !== changePassword.newPassword) {
+            setErrorMessage('New password and confirm new password do not match');
         } else {
-            setErrorMessage("");
+            setErrorMessage('');
         }
+    };
+
+    function emitSaveData(event: any) {
+        event.preventDefault();
+        props.onSaveHandler && props.onSaveHandler(changePassword);
+        setChangePassword({ 
+            currentPassword: "", 
+            newPassword: "", 
+            newPasswordConfirm: "" ,
+        });
     }
-    function handlePasswordDataSubmit(data: any) {
-        props.PasswordDataSubmit(data);
-    }
 
-    const isPasswordValid = (password: any) => {
-        if (!password || password.length === 0) {
-            return false;
-        }
-        return true;
-    };
+        const isPasswordValid = (password: any) => {
+            if (!password || password.length === 0) {
+                return false;
+            }
+            return true;
+        };
 
-    const isNewPasswordValid = (password: any) => {
-        if (!password || password.length === 0) {
-            return false;
-        }
-        return true;
-    };
+        const isNewPasswordValid = (password: any) => {
+            if (!password || password.length === 0) {
+                return false;
+            }
+            return true;
+        };
 
-    const isConfirmedPasswordValid = (password: any) => {
-        if (!password || password !== changePassword?.newPassword) {
-            return false;
-        }
-        return true;
-    };
-    const FormValid = isPasswordValid(changePassword?.currentPassword) && isNewPasswordValid(changePassword?.newPassword) && isConfirmedPasswordValid(changePassword?.newPasswordConfirm);
+        const isConfirmedPasswordValid = (password: any) => {
+            if (!password || password !== changePassword?.newPassword) {
+                return false;
+            }
+            return true;
+        };
+        const FormValid = isPasswordValid(changePassword?.currentPassword) && isNewPasswordValid(changePassword?.newPassword) && isConfirmedPasswordValid(changePassword?.newPasswordConfirm);
+
 
     return (
         <form>
-             <div className="custom-content-scroll">
-        <div className="row">
-            <div className="col-12 col-md-12">
-                <RdsInput
-                    size="medium"
-                    label="Current Password"
-                    inputType="password"
-                    isDisabled={false}
-                    readonly={false}
-                    placeholder="Current Password"
-                    value={changePassword?.currentPassword}
-                    onChange={(e: any) => setCurrentPassword(e.target.value)}
-                    required={true}
-                ></RdsInput>
-            </div>
-            <div className="col-12 col-md-12">
-                <RdsInput
-                    size="medium"
-                    label="New Password"
-                    inputType="password"
-                    isDisabled={false}
-                    readonly={false}
-                    required={true}
-                    placeholder="New Password"
-                    value={changePassword?.newPassword}
-                    onChange={(e: any) => setNewPassword(e.target.value)}
-                ></RdsInput>
-                {/* <div className="form-control-feedback">
+            <div className="custom-content-scroll">
+                <div className="row">
+                    <div className="col-12 col-md-12">
+                        <RdsInput
+                            size="medium"
+                            label="Current Password"
+                            inputType="password"
+                            isDisabled={false}
+                            readonly={false}
+                            placeholder="Current Password"
+                            value={changePassword?.currentPassword}
+                            onChange={(e: any) => handleDataChanges(e.target.value, "currentPassword")}
+                            required={true}
+                        ></RdsInput>
+                    </div>
+                    <div className="col-12 col-md-12">
+                        <RdsInput
+                            size="medium"
+                            label="New Password"
+                            inputType="password"
+                            isDisabled={false}
+                            readonly={false}
+                            required={true}
+                            placeholder="New Password"
+                            value={changePassword?.newPassword}
+                            onChange={(e: any) => handleDataChanges(e.target.value, "newPassword")}
+                        ></RdsInput>
+                        {/* <div className="form-control-feedback">
 					{errorPatternMessage && (<span className="text-danger">{errorPatternMessage}</span>)}
 				</div> */}
-            </div>
-            <div className="col-12 col-md-12">
-                <RdsInput
-                    size="medium"
-                    label="Confirm New Password"
-                    inputType="password"
-                    isDisabled={false}
-                    readonly={false}
-                    placeholder="Confirm New Password"
-                    value={changePassword?.newPasswordConfirm}
-                    onChange={(e: any) => setConfirmNewPassword(e.target.value)}
-                ></RdsInput>
-                <div className="form-control-feedback">
-                    {errorMessage && (<span className="text-danger">{errorMessage}</span>)}
+                    </div>
+                    <div className="col-12 col-md-12">
+                        <RdsInput
+                            size="medium"
+                            label="Confirm New Password"
+                            inputType="password"
+                            isDisabled={false}
+                            readonly={false}
+                            required={true}
+                            placeholder="Confirm New Password"
+                            value={changePassword?.newPasswordConfirm}
+                            onChange={(e: any) => handleDataChanges(e.target.value, "newPasswordConfirm")}
+                        ></RdsInput>
+                        <div className="form-control-feedback">
+                            {errorMessage && (<span className="text-danger">{errorMessage}</span>)}
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
             </div>
             <div className="d-flex flex-column-reverse flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3">
                 <RdsButton
@@ -114,10 +117,10 @@ const RdsCompChangeUserPassword = (props: RdsChangePasswordProps) => {
                     block={false}
                     type="submit"
                     size="small"
-                    onClick={() => handlePasswordDataSubmit(changePassword)}
+                    onClick={(e: any) => emitSaveData(e)}
                     isDisabled={!FormValid}
                 />
-            </div>        
+            </div>
         </form>
     );
 
