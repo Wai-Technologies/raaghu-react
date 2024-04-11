e6b9a63ca081eefd7071c700b7d865932fe005e2
AkshayPotphode <155605401+AkshayPotphode@users.noreply.github.com>
2024-04-11 17:52:45 +0530

    Enhancement on Secrets and Notification Setting components.

--- raaghu-components/src/rds-comp-secrets/rds-comp-secrets.stories.tsx
+++ raaghu-components/src/rds-comp-secrets/rds-comp-secrets.stories.tsx
@@ -21,7 +21,7 @@ export const Default: Story = {
         
     }
 } satisfies Story;
-Default.parameters = { controls: { include: [] } };
+// Default.parameters = { controls: { include: [] } };
 
 
 
