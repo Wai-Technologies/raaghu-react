d7f581ac3991deba767a79ad15f16814e12f937a
Supriya Tambe <supriya.tambe@waiin.com>
2024-04-19 18:35:51 +0530

    Commit changes for upgrade 7 version to 8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    // "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../raaghu-elements/src/**/*.stories.@(js|jsx|ts|tsx)",
     "../raaghu-components/**/**/*.stories.@(js|jsx|ts|tsx)",
-    "../raaghu-layouts/**/**/*.stories.@(js|jsx|ts|tsx)",
+    "../raaghu-layouts/**/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
@@ -14,6 +13,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/preset-scss",
+    "@storybook/addon-docs",
     //"@storybook/addon-toolbars"
     "storybook-addon-themes",
   ],
