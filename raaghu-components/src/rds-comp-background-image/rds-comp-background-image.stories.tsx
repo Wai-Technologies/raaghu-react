158403c43bf71b719f6c007f8665d64d158639b8
rameshwarkamandar <155446782+rameshwarkamandar@users.noreply.github.com>
2024-04-22 19:20:10 +0530

    bug 247: Resolve  Issues on Component Background image

--- raaghu-components/src/rds-comp-background-image/rds-comp-background-image.stories.tsx
+++ raaghu-components/src/rds-comp-background-image/rds-comp-background-image.stories.tsx
@@ -18,11 +18,11 @@ type Story = StoryObj<typeof RdsCompBackgroundImage>;
 
 export const Default: Story = {
     args: {
-            imageUrl: "https://cdn.pixabay.com/photo/2020/12/18/16/56/laptop-5842509_960_720.jpg",
-            title: "New arrivals are here",
-            btnLabel: "CHECK NEW ARRIVALS HERE",
-            subtitle: "The new arrivals have, well newly arrived. Check out the latest options from our summer small-batch release while they are still in stock.",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
+        imageUrl: "https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg",
+        title: "New arrivals are here",
+        btnLabel: "CHECK NEW ARRIVALS HERE",
+        subtitle: "The new arrivals have, well newly arrived. Check out the latest options from our summer small-batch release while they are still in stock.",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
     }
 } satisfies Story;
