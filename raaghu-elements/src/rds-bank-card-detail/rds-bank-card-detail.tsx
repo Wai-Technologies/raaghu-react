d14a6442bd2762599a9360f7c05c2a1a346849f3
AkshayPotphode <155605401+AkshayPotphode@users.noreply.github.com>
2024-04-26 14:51:51 +0530

    Resolved issues on Detail List component

--- raaghu-elements/src/rds-bank-card-detail/rds-bank-card-detail.tsx
+++ raaghu-elements/src/rds-bank-card-detail/rds-bank-card-detail.tsx
@@ -24,10 +24,10 @@ const RdsBankCardDetail = (props: RdsBankCardDetailProps) => {
 
     return (
         <Fragment>
-            <div className="m-1 p-1 ">
+            <div className="m-1 p-1">
                 {props.cardDatas.map((data: any, index: number) => (
                     <div key={index} onClick={() => clickHandler(index)}>
-                        <div className=" mb-4">
+                        <div className="cursor-pointer mb-4">
                             <div
                                 className={`${activeButton === index ? "border__color p-4" : "border__gray p-4"
                                     }`}
@@ -35,7 +35,7 @@ const RdsBankCardDetail = (props: RdsBankCardDetailProps) => {
                             >
                                 <div className=" ">
                                     <div className="d-flex justify-content-between">
-                                        <div className="d-flex  align-items-center">
+                                        <div className="d-flex align-items-center">
                                             <RdsIcon name={data.icon}
                                                 height={data.iconHeight}
                                                 width={data.iconWidth}
@@ -44,13 +44,13 @@ const RdsBankCardDetail = (props: RdsBankCardDetailProps) => {
                                                 colorVariant={data.iconColorVarient}
                                             />
 
-                                            <div className="ms-3  ">
+                                            <div className="ms-3">
                                                 <div className="ms-3 d-flex fs-6">
                                                     <span>{data.cardName}</span>
-                                                    <span className="ms-1 me-1 ">Ending with</span>
+                                                    <span className="ms-1 me-1">Ending with</span>
                                                     <span>{data.cardNumber}</span>
                                                 </div>
-                                                <div className=" ms-3 fs-7">
+                                                <div className="ms-3 fs-7">
                                                     <span className="text-muted fs-7">
                                                         {" "}
                                                         Expiry {data.cardExpiry}
@@ -66,7 +66,7 @@ const RdsBankCardDetail = (props: RdsBankCardDetailProps) => {
                                                     type="radio"
                                                     name="flexRadioDefault"
                                                     id="flexRadioD"
-                                                    checked
+                                                    checked={activeButton === index}
                                                 />) :
                                                 (
                                                     <input
@@ -82,26 +82,26 @@ const RdsBankCardDetail = (props: RdsBankCardDetailProps) => {
                                 </div>
 
                                 {props.isEditable == true && (
-                                    <div className="mt-2  ms-5">
+                                    <div className="mt-2 ms-5">
                                         {activeButton == index ? (clicked == false ? (<a
-                                            className="ms-3 text-primary text-decoration-none "
+                                            className="ms-3 text-primary text-decoration-none"
                                             onClick={setDefaultHandler}
                                         >
                                             Set as default
                                         </a>
                                         ) : (<a
-                                            className=" ms-3 text-muted  me-1 text-decoration-none"
+                                            className="ms-3 text-muted me-1 text-decoration-none"
                                             onClick={setDefaultHandler}
                                         >
                                             Default
                                         </a>)) : (<a
-                                            className="ms-3 text-primary text-decoration-none "
+                                            className="ms-3 text-primary text-decoration-none"
                                         >
                                             Set as default
                                         </a>)}
 
                                         <a
-                                            className="text-primary ms-2 text-decoration-none "
+                                            className="text-primary ms-2 text-decoration-none"
                                             style={{ textDecoration: "auto" }}
                                         >
                                             Edit
