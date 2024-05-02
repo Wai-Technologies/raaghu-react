26cc6b4fd0423cf8a043f67ddc55ad6d1bb28082
Kaushik Deepak Gokhale <kaushik.gokhale@waiin.com>
2024-05-02 12:56:55 +0530

    Updated file for initial page

--- raaghu-pages/src/App.tsx
+++ raaghu-pages/src/App.tsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <a href="https://raaghu.io" target="_blank">
+          <img src="https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/Raaghu%20Logo%20SD.svg" className="logo" alt="Raaghu Logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React Logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Raaghu with React + Vite</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Start creating pages by using Application Shells, Layouts, Component, Elements, Charts and more.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Raaghu, Vite and React Logos to learn more
       </p>
     </>
   )
