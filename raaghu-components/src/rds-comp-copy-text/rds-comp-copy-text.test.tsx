8c497a8cbdd93e31f951af79d89103063d79dd4c
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-06-28 16:43:45 +0530

    resolved issue

--- raaghu-components/src/rds-comp-copy-text/rds-comp-copy-text.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import rds-comp-copy-text from './rds-comp-copy-text';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<rds-comp-copy-text />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
