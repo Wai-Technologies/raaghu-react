00b656d7820abcf5883175ed7d4a44481c0cbffb
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2024-06-28 15:34:04 +0530

    Remove test file in storybook

--- raaghu-components/src/rds-comp-premium-support/rds-comp-premium-support.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import rds-comp-premium-support from './rds-comp-premium-support';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<rds-comp-premium-support />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
