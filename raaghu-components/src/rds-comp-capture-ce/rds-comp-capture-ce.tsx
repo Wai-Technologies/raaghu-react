69ffaf2db76f234a4c085a7e60446cd4fe993964
Piyush Gulhane <piyush.gulhane@6clicks.com>
2024-07-15 12:43:52 +0530

    min video length defined, minor UI changes

--- raaghu-components/src/rds-comp-capture-ce/rds-comp-capture-ce.tsx
+++ raaghu-components/src/rds-comp-capture-ce/rds-comp-capture-ce.tsx
@@ -141,10 +141,14 @@ const RdsCompCaptureCe: React.FC<RdsCompCaptureCeProps> = (props) => {
         screen: true,
         audio: false,
         onStop: async (blobUrl, videoToAdd) => {
-            const videoName = `Video_${videos.length + 1}`;
-            setVideos((existingVideosArray) => [...existingVideosArray, videoToAdd]);
-            const videoData = await getBase64Image(videoToAdd);
-            localStorage.setItem(videoName, videoData);
+            if (videoToAdd && videoToAdd.size > 0 && videoToAdd.size / videoToAdd.type.length > 10 * 1024) {
+                const videoName = `Video_${videos.length + 1}`;
+                setVideos((existingVideosArray) => [...existingVideosArray, videoToAdd]);
+                const videoData = await getBase64Image(videoToAdd);
+                localStorage.setItem(videoName, videoData);
+            } else {
+                alert("Video length should be more than 10 seconds.");
+            }
         },
     });
 
@@ -331,34 +335,26 @@ const RdsCompCaptureCe: React.FC<RdsCompCaptureCeProps> = (props) => {
                             <div className="mb-1">
                                 <div className="d-flex flex-wrap gap-3">
                                     {screenshots.map((screenshot, index) => (
-                                        <div key={index} style={{ background: "#f7f7f7", borderRadius: "5px", display: "flex", alignItems: "center", justifyContent: "center", padding: "7px" }}>
+                                        <div key={index} style={{ background: "#f7f7f7", borderRadius: "5px", display: "flex", alignItems: "center", padding: "7px", paddingLeft: "7px", paddingRight: "7px", width: "auto" }}>
                                             <div id="screenshotDiv" onClick={(e) => handleScreenshotOrVideoClick(e, index, undefined, screenshot)}>
-                                                <span style={{ display: "flex", alignItems: "center", justifyContent: "center", paddingRight: "7px" }}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera" viewBox="0 0 16 16">
-                                                        <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z" />
-                                                        <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0" />
-                                                    </svg>
-                                                </span>
-                                                <span style={{ display: "flex", alignItems: "center", justifyContent: "center", paddingRight: "7px" }}>{`Screenshot_${index + 1}`}</span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera" viewBox="0 0 16 16">
+                                                    <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z" />
+                                                    <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0" />
+                                                </svg>
+                                                <span style={{ paddingLeft: "7px", paddingRight: "7px" }}>{`Screenshot_${index + 1}`}</span>
                                             </div>
-                                            <span style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                                <button id="deleteScreenshot" type="button" className="btn-close btn-danger" style={{ width: "7px", height: "7px" }} onClick={() => handleDeleteScreenshot(index)} />
-                                            </span>
+                                            <button id="deleteScreenshot" type="button" className="btn-close btn-danger" style={{ width: "7px", height: "7px" }} onClick={() => handleDeleteScreenshot(index)} />
                                         </div>
                                     ))}
                                     {videos.map((video, index) => (
-                                        <div key={index} style={{ background: "#f7f7f7", borderRadius: "5px", display: "flex", alignItems: "center", justifyContent: "center", padding: "7px" }}>
+                                        <div key={index} style={{ background: "#f7f7f7", borderRadius: "5px", display: "flex", alignItems: "center", padding: "7px", paddingLeft: "7px", paddingRight: "7px", width: "auto" }}>
                                             <div id="videoDiv" onClick={(e) => handleScreenshotOrVideoClick(e, index, video, undefined)}>
-                                                <span style={{ display: "flex", alignItems: "center", justifyContent: "center", paddingRight: "7px" }}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera-video" viewBox="0 0 16 16">
-                                                        <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z" />
-                                                    </svg>
-                                                </span>
-                                                <span style={{ display: "flex", alignItems: "center", justifyContent: "center", paddingRight: "7px" }}>{`Video_${index + 1}`}</span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-camera-video" viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z" />
+                                                </svg>
+                                                <span style={{ paddingLeft: "7px", paddingRight: "7px" }}>{`Video_${index + 1}`}</span>
                                             </div>
-                                            <span style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                                                <button id="deleteVideo" type="button" className="btn-close btn-danger" style={{ width: "7px", height: "7px" }} onClick={() => handleDeleteVideo(index)} />
-                                            </span>
+                                            <button id="deleteVideo" type="button" className="btn-close btn-danger" style={{ width: "7px", height: "7px" }} onClick={() => handleDeleteVideo(index)} />
                                         </div>
                                     ))}
                                     {
