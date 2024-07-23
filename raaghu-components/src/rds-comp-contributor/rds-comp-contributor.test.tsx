a65b8768131ebee14a3abd926ad88ea0b2fd223a
hemalatagaikwad <98730209+hemalatagaikwad@users.noreply.github.com>
2024-07-23 11:45:25 +0530

    restructure code as per review

--- raaghu-components/src/rds-comp-contributor/rds-comp-contributor.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import rds-comp-contributor from './rds-comp-contributor';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<rds-comp-contributor />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
