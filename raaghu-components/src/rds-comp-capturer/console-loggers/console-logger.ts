096edb651ad890f7ebf3b00cb0a791fa0b1169f9
Piyush Gulhane <piyush.gulhane@6clicks.com>
2024-07-25 15:25:25 +0530

    multiple console errors in one line

--- raaghu-components/src/rds-comp-capturer/console-loggers/console-logger.ts
+++ raaghu-components/src/rds-comp-capturer/console-loggers/console-logger.ts
@@ -1,17 +1,32 @@
-interface ConsoleMessage {
-    message: string;
-    optionalParams: any[];
-    stack: string | undefined;
-}
+// interface ConsoleMessage {
+//     message: string;
+//     optionalParams: any[];
+//     stack: string | undefined;
+// }
 
-const consoleErrors: ConsoleMessage[] = [];
+// const consoleErrors: ConsoleMessage[] = [];
+
+// const originalConsoleError = console.error;
+
+// console.error = (message?: any, ...optionalParams: any[]): void => {
+//     const stack = new Error().stack;
+//     consoleErrors.push({ message, optionalParams, stack });
+//     originalConsoleError.apply(console, [message, ...optionalParams]);
+// };
+
+// export { consoleErrors };
+
+let consoleErrors = "";
 
 const originalConsoleError = console.error;
 
-console.error = (message?: any, ...optionalParams: any[]): void => {
+console.error = (message?: any): void => {
     const stack = new Error().stack;
-    consoleErrors.push({ message, optionalParams, stack });
-    originalConsoleError.apply(console, [message, ...optionalParams]);
+    const combinedMessage = `${message}${stack} \n`;
+
+    consoleErrors += combinedMessage;
+
+    originalConsoleError.apply(console, [message]);
 };
 
 export { consoleErrors };
