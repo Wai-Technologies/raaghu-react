85613cd593e56c3a31e211ab000657e3b6a60b05
AkshayPotphode <155605401+AkshayPotphode@users.noreply.github.com>
2024-07-26 09:51:20 +0530

    Resolved issues on Collapsible Container component

--- raaghu-components/src/rds-comp-collapsible-container/rds-comp-collapsible-container.tsx
+++ raaghu-components/src/rds-comp-collapsible-container/rds-comp-collapsible-container.tsx
@@ -1,87 +1,68 @@
 import React, { useState, ReactNode } from "react";
 import { RdsIcon } from "../rds-elements";
-import "./rds-comp-collapsible-container.style.css";
 
 export interface MenuItem {
-  id?: string;
-  name?: string;
-  icon?: string;
-  component?: ReactNode;
+   id?: string;
+   name?: string;
+   icon?: string;
+   component?: ReactNode; 
 }
 
 export interface RdsCompCollapsibleContainerProps {
-  position: "right" | "left";
-  menuItems: MenuItem[];
+   position: "right" | "left";
+   menuItems: MenuItem[];
 }
 
-const RdsCompCollapsibleContainer = (
-  props: RdsCompCollapsibleContainerProps
-) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+const RdsCompCollapsibleContainer = (props: RdsCompCollapsibleContainerProps) => {
+   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const toggleCollapse = () => {
-    setIsCollapsed(!isCollapsed);
-  };
+   const toggleCollapse = () => {
+      setIsCollapsed(!isCollapsed);
+   };
 
-  return (
-    <div
-      className={`wrapperContainer justify-content-md-start justify-content-xl-end ${props.position}`}
-    >
-      {props.position === "right" && (
-        <div className="leftChevronPosition">
-          <div className="chevron-container">
-            <RdsIcon
-              name={isCollapsed ? "chevron_left" : "chevron_right"}
-              width="22px"
-              height="11px"
-              onClick={toggleCollapse}
-              classes="collapse-button-for-container"
-            />
-          </div>
-        </div>
-      )}
-      <div
-        className={`container-collapse mb-4 mb-lg-0 ${
-          isCollapsed ? "collapsed-container" : ""
-        }`}
-      >
-        <div className="content">
-          {Array.isArray(props.menuItems) &&
-            props.menuItems.map((item, index) => (
-              <div className="menu-item-collapsible-container" key={index}>
-                {item.icon && (
+   return (
+      <div className={`wrapperContainer justify-content-md-start justify-content-xl-end ${props.position}`}>
+         {props.position === "right" &&
+            <div className="leftChevronPosition">
+               <div className="chevron-container">
                   <RdsIcon
-                    name={item.icon}
-                    width="20px"
-                    height="20px"
-                    fill={false}
-                    classes="mx-3"
-                    tooltip={true}
-                    tooltipTitle={item.name}
-                    tooltipPlacement="top"
+                     name={isCollapsed ? 'chevron_left' : 'chevron_right'}
+                     width="22px"
+                     height="11px"
+                     onClick={toggleCollapse}
+                     classes="collapse-button-for-container"
                   />
-                )}
-                {!isCollapsed && item.name && <span>{item.name}</span>}
-                <div className="px-3">{item.component && item.component}</div>
-              </div>
-            ))}
-        </div>
+               </div>
+            </div>
+         }
+         <div className={`container-collapse mb-4 mb-lg-0 d-flex flex-column bg-white border-0 overflow-hidden rounded-4 shadow visible ${isCollapsed ? 'collapsed-container' : ''}`}>
+            <div className="content">
+               {Array.isArray(props.menuItems) && props.menuItems.map((item, index) => (
+                  <div className="menu-item-collapsible-container" key={index}>
+                     {item.icon && <RdsIcon name={item.icon} width="20px" height="20px" fill={false} classes="mx-3" />}
+                     {!isCollapsed && item.name && <span>{item.name}</span>}
+                     <div className="px-3">
+                     {item.component && item.component}
+                     </div>
+                  </div>
+               ))}
+            </div>
+         </div>
+         {props.position === "left" &&
+            <div className="rightChevronPosition">
+               <div className="chevron-container">
+                  <RdsIcon
+                     name={isCollapsed ? 'chevron_right' : 'chevron_left'}
+                     width="22px"
+                     height="11px"
+                     onClick={toggleCollapse}
+                     classes="collapse-button-for-container"
+                  />
+               </div>
+            </div>
+         }
       </div>
-      {props.position === "left" && (
-        <div className="rightChevronPosition">
-          <div className="chevron-container">
-            <RdsIcon
-              name={isCollapsed ? "chevron_right" : "chevron_left"}
-              width="22px"
-              height="11px"
-              onClick={toggleCollapse}
-              classes="collapse-button-for-container"
-            />
-          </div>
-        </div>
-      )}
-    </div>
-  );
+   );
 };
 
 export default RdsCompCollapsibleContainer;
