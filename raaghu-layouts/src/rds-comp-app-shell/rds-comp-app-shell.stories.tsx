8eb23a893eb30037ceef09dd82a5b5480b4833a7
prakashmhaske <prakash.mhaske@waiin.com>
2024-08-30 12:36:42 +0530

    Update rds-comp-app-shell.stories.tsx

--- raaghu-layouts/src/rds-comp-app-shell/rds-comp-app-shell.stories.tsx
+++ raaghu-layouts/src/rds-comp-app-shell/rds-comp-app-shell.stories.tsx
@@ -5,6 +5,7 @@ import RdsCompAppShellItem from "./rds-comp-app-shell-item";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import RdsCompSideNavigation from "../../../raaghu-components/src/rds-comp-side-navigation";
 import RdsCompTopNavigation from "../../../raaghu-components/src/rds-comp-top-navigation/rds-comp-top-navigation";
+import { RdsIcon } from "../../../raaghu-elements/src";
 
 const meta: Meta = {
   title: "Application Shells",
@@ -16,12 +17,17 @@ const meta: Meta = {
   argTypes: {},
 } satisfies Meta<typeof RdsCompAppShell>;
 
+const toggleDropdown = () => {
+  console.log('Dropdown clicked');
+};
+
 export default meta;
 type Story = StoryObj<typeof RdsCompAppShell>;
 
 export const Classic: Story = {
   args: {
     displayType: "Classic",
+  
     children: (
       <>
         <RdsCompAppShellItem title={""}>
@@ -96,7 +102,7 @@ export const Classic: Story = {
               profileTitle="John Doe"
               profileEmail="john.doe@raaghu.io"
               profileName="John Doe"
-              
+
               themeItems={[
                 {
                   icon: "sun",
@@ -236,3 +242,206 @@ export const Classic: Story = {
     ),
   },
 } satisfies Story;
+
+
+
+export const Advance: Story = {
+  args: {
+    displayType: "Advance",
+    children: (
+      <>
+        <div className="container-fluid bg-white px-0">
+          <div>
+            <nav
+              className={
+                "navbar shadow d-block"
+              }
+            >
+              <div>
+                <div>
+                  {/* mobile top nav */}
+
+                  <div className="d-md-none d-block">
+                    <button
+                      className="navbar-toggler d-xxl-none d-xl-none d-lg-none d-md-none d-none border-0"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="d-flex justify-content-between">
+                      <div
+                        id="raaghuLogo"
+                        className=""
+                      >
+                        <img
+                          className="cursor-pointer tenant-sidenav-logo"
+                          src="https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/raaghu-design-system-lightmode.png"
+                          alt="logo"
+                          height="45px"
+                        ></img>
+                      </div>
+
+                      <div className="align-items-center d-flex ms-auto">
+                        <div className="px-2 px-md-3">
+                          <div className="dropdown">
+
+                            <button className="align-items-center bg-transparent border-0 d-flex dropdown-toggle justify-content-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              <div className="px-2">
+                                {/* <RdsAvatar
+                                                                firstName="Hi"
+                                                                lastName={props.profileName}
+                                                                profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp=CAU"
+                                                                role=""
+                                                                size="small"
+                                                                titleAlign="horizontal"
+                                                                withProfilePic
+                                                            /> */}
+                                <img
+                                  src="https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/raaghu-design-system-lightmode.png"
+                                  alt={"profilePic"}
+                                  width="30px"
+                                  height="30px"
+                                  className="profil_image_Class rounded-circle"
+                                  data-testid="avatar"
+                                  style={{ height: '-webkit-fill-available' }}
+                                ></img>
+                                <span className="ps-2">Hi, <span className="fw-bolder"></span></span>
+                              </div>
+                            </button>
+                            <ul className="dropdown-menu mt-2 avatar-dropdown">
+                              <li><a className="dropdown-item" >My Account</a></li>
+                              <li><a className="dropdown-item" >Logout</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* end */}
+
+                  <div className="d-md-flex d-grid align-items-center justify-content-md-between right-side-menu tenant-sm-nav">
+                    <div className="position-relative px-2 px-lg-3 d-md-flex d-block justify-content-center align-items-center text-center">
+                      <div
+                        id="raaghuLogo"
+                        className="d-xxl-block d-xl-block d-lg-block d-md-block d-none"
+
+                      >
+                        <img
+                          className="cursor-pointer tenant-sidenav-logo"
+                          src="https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/raaghu-design-system-lightmode.png"
+                          alt="logo"
+                          height="45px"
+                        ></img>
+                      </div>
+                      <div className="ps-lg-4 d-flex pe-md-0 px-4 justify-content-between gap-4 ms-lg-4 top-link">
+                        <div className="m-lg-2 cursor-pointer fw-semibold">
+                          <a
+                            href="https://react.raaghu.ai"
+                            target="_blank"
+                            className="text-black"
+                          >
+                            <span className="d-md-block d-none">Storybook</span>
+                            <span className="d-md-none b-block">
+                              <RdsIcon
+                                colorVariant="dark"
+                                height="20px"
+                                name="code"
+                                stroke
+                                width="20px"
+                              />
+                            </span>
+                          </a>
+                        </div>
+                        <div className="m-lg-2 cursor-pointer fw-semibold">
+                          <a
+                            href="https://docs.raaghu.ai"
+                            target="_blank"
+                            className="text-black"
+                          >
+                            <span className="d-md-block d-none">Documentation</span>
+                            <span className="d-md-none b-block">
+                              <RdsIcon
+                                colorVariant="dark"
+                                height="20px"
+                                name="file_data"
+                                stroke
+                                width="20px"
+                              />
+                            </span>
+                          </a>
+                        </div>
+                        <div className="m-lg-2 cursor-pointer fw-semibold"
+                        >
+                          <a href="#" className="text-black">
+                            <span className="d-md-block d-none">Download Project</span>
+                            <span className="d-md-none b-block">
+                              <RdsIcon
+                                colorVariant="dark"
+                                height="20px"
+                                name="download_data"
+                                stroke
+                                width="20px"
+                              />
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="align-items-center d-flex ms-auto d-none d-md-block">
+                      <div className="px-2 px-md-3 d-none d-md-block">
+
+                        <div className="dropdown">
+
+                          <button className="align-items-center bg-transparent border-0 d-flex dropdown-toggle justify-content-center" type="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={toggleDropdown}>
+                            <div className="px-2">
+                              {/* <RdsAvatar
+                                                            firstName="Hi"
+                                                            lastName={props.profileName}
+                                                            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp=CAU"
+                                                            role=""
+                                                            size="small"
+                                                            titleAlign="horizontal"
+                                                            withProfilePic
+                                                        /> */}
+                              <img
+                                src="./assets/profile-picture-circle.svg"
+                                alt={"profilePic"}
+                                width="30px"
+                                height="30px"
+                                className="profil_image_Class rounded-circle"
+                                data-testid="avatar"
+                                style={{ height: '-webkit-fill-available' }}
+                              ></img>
+                              <span className="ps-2">Hi, <span className="fw-bolder"></span></span>
+                            </div>
+                          </button>
+
+                          <ul className="dropdown-menu mt-2 avatar-dropdown">
+                            <li><a className="dropdown-item">My Account</a></li>
+                            <li><a className="dropdown-item">Logout</a></li>
+                          </ul>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+
+          <div className=" align-items-center bg-body-secondary d-flex justify-content-center m-3 h-300px">
+          <h2>Add Layout Here</h2>
+        </div>                
+      </>
+    ),
+  },
+} satisfies Story;
+
