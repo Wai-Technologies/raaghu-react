1d8c14c3a2680bd5a52044b992a1e033e3de9375
prakashmhaske <prakash.mhaske@waiin.com>
2024-09-27 11:34:15 +0530

    commit changes for Select list implement multiselect with icon

--- raaghu-elements/src/rds-select-list/rds-select-list.stories.tsx
+++ raaghu-elements/src/rds-select-list/rds-select-list.stories.tsx
@@ -22,24 +22,24 @@ type Story = StoryObj<typeof RdsSelectList>;
 export const Default: Story = {
     args: {
         id: "story",
-        label: "Open select list",
+        label: "Default Select List",
         isMultiple: false,
-        placeholder: "Select option",
+        placeholder: "Select label",
         selectItems: [
             {
-                option: "One",
+                label: "One",
                 value: "one"
             },
             {
-                option: "two",
+                label: "two",
                 value: "two"
             },
             {
-                option: "three",
+                label: "three",
                 value: "three"
             },
             {
-                option: "four",
+                label: "four",
                 value: "four"
             }
 
@@ -57,27 +57,26 @@ Default.parameters = { controls: { include: ['id', 'label', 'isMultiple', 'place
 export const Multiple: Story = {
     args: {
         id: "story",
-        label: "Open select list",
+        label: "Multiple Select List",
         isMultiple: true,
-        placeholder: "Select option",
+        placeholder: "Select label",
         selectItems: [
             {
-                option: "One",
+                label: "One",
                 value: "one"
             },
             {
-                option: "two",
+                label: "two",
                 value: "two"
             },
             {
-                option: "three",
+                label: "three",
                 value: "three"
             },
             {
-                option: "four",
+                label: "four",
                 value: "four"
             }
-
 
         ],
         isDisabled: false,
@@ -87,3 +86,62 @@ export const Multiple: Story = {
     }
 }
 Multiple.parameters = { controls: { include: ['id', 'label', 'isMultiple', 'placeholder', 'selectItems', 'isDisabled', 'required', 'isSearchable', 'isBold', 'onChange'] } };
+
+export const WithIcon: Story = {
+    args: {
+        id: "story",
+        label: "Select List With Icon",
+        isMultiple: true,
+        defaultImgUrl:"https://www.svgrepo.com/show/497407/profile-circle.svg",
+        placeholder: "Select label",      
+        selectItems: [
+            {
+                label: "Riya Sharma",
+                value: "Riya",
+                imgUrl: "https://th.bing.com/th/id/OIP.dmCI62ZlmvIp1yxyBVSAxAHaHP?w=567&h=555&rs=1&pid=ImgDetMain",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            },
+            {
+                label: "John Doe",
+                value: "John",
+                imgUrl: "https://i.vimeocdn.com/portrait/20107774_640x640",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            },
+            {
+                label: "Richard P",
+                value: "Richard",
+                imgUrl: "https://www.mockofun.com/wp-content/uploads/2019/12/circle-profile-pic-768x730.jpg",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            },
+            {
+                label: "Alex Brown",
+                value: "Alex",
+                imgUrl: "https://i0.wp.com/studio.ameliate.com/wp-content/uploads/2021/01/Michele-Oval-Edited-768x768-1.png?w=760&ssl=1",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            },
+            {
+                label: "Chris Johnson",
+                value: "Chris",
+                imgUrl: "",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            },
+            {
+                label: "Elizabeth Diva",
+                value: "Elizabeth",
+                imgUrl: "https://th.bing.com/th/id/OIP.GmUT02hCOJSpUjLmUtV5hAHaHa?rs=1&pid=ImgDetMain",
+                imgWidth: "30px",
+                imgHeight: "30px",
+            }
+        ],
+        isDisabled: false,
+        required: false,
+        isSearchable: true,
+        isBold: false
+    }
+}
+WithIcon.parameters = { controls: { include: ['id','label', 'placeholder', 'selectItems', 'isDisabled', 'required', 'isSearchable', 'isBold','defaultImgUrl'] } };
