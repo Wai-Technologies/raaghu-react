8fc20a3b770638052278e9bd784cc135e1fc0244
Rahul Patil <55730676+rahulp34@users.noreply.github.com>
2024-10-10 11:50:02 +0530

    commited changes

--- raaghu-elements/src/rds-toast/rds-toast.tsx
+++ raaghu-elements/src/rds-toast/rds-toast.tsx
@@ -53,7 +53,7 @@ const RdsToast = (props: RdsToastProps) => {
                 >
                     {props.showHeader && (
                         <div>
-                            <div className="toast-header p-2 d-flex justify-content-between align-items-end">
+                            <div className="toast-header d-flex justify-content-between align-items-end pb-0">
                                 <div className="me-2">
                                     {props.withIcon && (
                                         <RdsIcon
@@ -90,7 +90,7 @@ const RdsToast = (props: RdsToastProps) => {
                                     <label className="progress-label ml-4">{props.progressWidth}%</label>
                                 </div>
                                 <label className="filename">{props.filename}</label>
-                                <div className="d-flex toast-footer justify-content-end pt-3 pb-1 pe-4">                                    
+                                <div className="d-flex toast-footer justify-content-end pb-1 pe-4">                                    
                                     <button type="button" className="btn text-primary btn-sm">Cancel</button>
                                     <button type="button" className="btn btn-primary btn-sm">Go To Downloads</button>
                                 </div>
@@ -100,14 +100,14 @@ const RdsToast = (props: RdsToastProps) => {
                                 <div className="d-flex ml-2 pl-2">
                                     <input type="text" className="form-control form-text pl-1" placeholder={props.placeholder} />
                                 </div>
-                                <div className="d-flex toast-footer justify-justify-content-start pt-3 pb-1 ps-2">
+                                <div className="d-flex toast-footer justify-justify-content-start">
                                     <button type="button" className="btn btn-primary btn-sm">Reply</button>
                                     <button type="button" className="btn text-primary btn-sm">Mark As Read</button>
                                 </div>
                             </div>
 
                             <div className={`toast-footer justify-content-end align-items-end ${props.layout === "request" ? "d-block" : "d-none"}`}>
-                                <div className="d-flex toast-footer justify-content-end pt-3 pb-1  pe-4">
+                                <div className="d-flex toast-footer justify-content-end">
                                     <button type="button" className="btn text-primary btn-sm">Reject</button>
                                     <button type="button" className="btn btn-primary btn-sm">Accept</button>
                                 </div>
@@ -139,7 +139,7 @@ const RdsToast = (props: RdsToastProps) => {
                                 ></button>                                                                
                             </div>
 
-                            <div className={`toast-footer justify-content-end align-items-end ${props.layout === "download" ? "d-block" : "d-none"}`}>
+                            <div className={`toast-footer pt-0 justify-content-end align-items-end ${props.layout === "download" ? "d-block" : "d-none"}`}>
                                 <div className="d-flex ml-4">
                                     <div className="progress w-100" aria-valuenow={props.progressWidth} aria-valuemin={0} aria-valuemax={100}>
                                         <div className="progress-bar btn-primary" role="progressbar" 
@@ -151,22 +151,22 @@ const RdsToast = (props: RdsToastProps) => {
                                     <label className="progress-label justify-content-end ml-3">{props.progressWidth}%</label>
                                 </div>
                                 <label className="filename">{props.filename}</label>
-                                <div className="d-flex toast-footer justify-content-end pt-3 pb-1  pe-4">
+                                <div className="d-flex toast-footer justify-content-end pe-4">
                                     <button type="button" className="btn text-primary btn-sm">Cancel</button>
                                     <button type="button" className="btn btn-primary btn-sm">Go To Downloads</button>
                                 </div>
                             </div>
 
-                            <div className={`toast-footer justify-content-end align-items-end ${props.layout === "chat" ? "d-block" : "d-none"}`}>
+                            <div className={`toast-footer pt-0 justify-content-end align-items-end ${props.layout === "chat" ? "d-block" : "d-none"}`}>
                                 <div className="d-flex">
                                     <input type="text" className="form-control form-text pl-1" placeholder={props.placeholder} />
                                 </div>
-                                <div className="d-flex toast-footer justify-justify-content-start pt-3 pb-1 ps-2">
+                                <div className="d-flex toast-footer justify-justify-content-start pb-1 ps-2">
                                     <button type="button" className="btn btn-primary btn-sm">Reply</button>
                                     <button type="button" className="btn text-primary btn-sm">Mark As Read</button>
                                 </div>
                             </div>
-                            <div className={`d-flex toast-footer justify-content-end align-items-end pt-3 pb-1 pe-4 ${props.layout === "request" ? "d-block" : "d-none"}`}>
+                            <div className={`pt-0 d-flex toast-footer justify-content-end align-items-end pb-1 pe-4 ${props.layout === "request" ? "d-block" : "d-none"}`}>
                                 <button type="button" className="btn text-primary btn-sm">Reject</button>
                                 <button type="button" className="btn btn-primary btn-sm">Accept</button>
                             </div>
