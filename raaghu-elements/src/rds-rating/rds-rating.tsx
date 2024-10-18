c0e98536f0f4ce5413ca5716be98b08dbe04edf9
Rahul Patil <55730676+rahulp34@users.noreply.github.com>
2024-10-18 14:27:37 +0530

    commited changes- 1] added two new icons for stars half filled and empty start in icon.tsx file 2] change the structure of code

--- raaghu-elements/src/rds-rating/rds-rating.tsx
+++ raaghu-elements/src/rds-rating/rds-rating.tsx
@@ -3,19 +3,19 @@ import "./rds-rating.css";
 import RdsIcon from "../rds-icon";
 
 export interface RdsRatingProps {
-  rating?: number,
-  colorVariant?: "primary" | "success" | "danger" | "warning" | "light" | "info" | "secondary" | "dark"
-  noOfReviews?: number
+  rating?: number;
+  colorVariant?: "primary" | "success" | "danger" | "warning" | "light" | "info" | "secondary" | "dark";
+  noOfReviews?: number;
   size?: string;
-  seeAllOption?: boolean
-  onSeeAll?: () => void
-  dataTestId?: string
+  seeAllOption?: boolean;
+  onSeeAll?: () => void;
+  dataTestId?: string;
 }
-
 const RdsRating = (props: RdsRatingProps) => {
-
+  const sizeClass = `${props.size === 'small' ? ' fs-5' : props.size === 'large' ? ' fs-3' : ' fs-4'}`;
   const [rating, setRating] = useState(props.rating || 0);
   const totalStars = 5;
+
   useEffect(() => {
     setRating(props.rating || 0);
   }, [props.rating]);
@@ -24,26 +24,64 @@ const RdsRating = (props: RdsRatingProps) => {
     setRating(rate);
   };
 
-  const sizeClass = `${props.size === 'small' ? ' fs-5' : props.size === 'large' ? ' fs-3' : ' fs-4'}`;
-
-  return (
-    <div className={`starrating align-items-center d-flex gap-2 ${sizeClass}`}>
-      <span className="fs-5 me-2 mt-2">{rating}</span>
-      {[...Array(totalStars)].map((_, i) => (
-        <RdsIcon
-          key={i}
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= totalStars; i++) {
+      if (i <= rating) {
+        stars.push(
+          <RdsIcon
           height={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
           width={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
           fill={true}
           stroke={false}
           name="star"
           colorVariant="review"
-          classes={`${i < rating ? `on text-${props.colorVariant}` : "off"}`}
-          onClick={() => handleRating(i + 1)}
-          isCursorPointer={true}
-        />
-      ))}
-      <span className="fs-6 mt-1"> See all {props.noOfReviews} review </span>
+
+            classes={`me-2 star filled text-${props.colorVariant}`}
+            onClick={() => handleRating(i)}
+          />
+        );
+      } else if (i === Math.ceil(rating) && rating % 1 !== 0) {
+        stars.push(
+          <RdsIcon
+          height={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
+          width={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
+          fill={true}
+          stroke={false}
+          colorVariant="review"
+
+            name="starhalf"
+            classes={`me-2 star half-filled text-${props.colorVariant}`}
+            onClick={() => handleRating(i)}
+          />
+        );
+      } else {
+        stars.push(
+          <RdsIcon
+          height={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
+          width={props.size === 'small' ? '14px' : props.size === 'large' ? '22px' : '19px'}
+          fill={true}
+          stroke={false}
+          colorVariant="review"
+            name="starempty"
+            classes={`me-2 star empty ${props.colorVariant}`}
+            onClick={() => handleRating(i)}
+          />
+        );
+      }
+    }
+    return stars;
+  };
+
+  return (
+    <div className="rds-rating d-flex" data-testid={props.dataTestId}>
+      {renderStars()}
+      {props.noOfReviews && <span  className="reviews fs-6 mt-1">({props.noOfReviews})</span>}
+      {props.seeAllOption && (
+        <button className="see-all fs-6 mt-1" onClick={props.onSeeAll}>
+          See All
+        </button>
+      )}
     </div>
   );
 };
