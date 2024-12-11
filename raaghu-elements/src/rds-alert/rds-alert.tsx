7bd5063ff7bae44ebfdeace3d9d010c1b149350a
atharvamachave28 <atharva.machave@waiin.com>
2024-12-11 11:03:25 +0530

    Replaced #code values with tokens

--- raaghu-elements/src/rds-alert/rds-alert.tsx
+++ raaghu-elements/src/rds-alert/rds-alert.tsx
@@ -4,7 +4,7 @@ import "./rds-alert.css";
 import RdsButton from "../rds-button";
 
 export interface RdsAlertProps {
-    type: "info" | "success" | "warning" | "error";    
+    type: "info" | "success" | "warning" | "error";
     dismisable?: boolean;
     icon?: string;
     iconFill?: boolean;
@@ -12,7 +12,7 @@ export interface RdsAlertProps {
     iconHeight?: string;
     iconWidth?: string;
     message?: string;
-    border?:"none" | "single" | "left border";
+    border?: "none" | "single" | "left border";
     delay?: number;
     position?: "top" | "bottom";
     onDismiss?: React.MouseEventHandler<HTMLButtonElement>;
@@ -24,7 +24,7 @@ export interface RdsAlertProps {
     title?: string;
     displayType?: "singleline" | "multiline";
     description?: string;
-  }
+}
 
 const RdsAlert = (props: RdsAlertProps) => {
     const [clicked, setClicked] = useState(false);
@@ -47,44 +47,44 @@ const RdsAlert = (props: RdsAlertProps) => {
     };
 
     const delayClass = `${clicked == true ? " d-none " : "w-100 "}`;
-    const colorType = props.type === "success" ? "primary" : props.type === "warning" ? "warning" : props.type === "error" ?  "danger" : "neutral";
+    const colorType = props.type === "success" ? "primary" : props.type === "warning" ? "warning" : props.type === "error" ? "danger" : "neutral";
 
     const classes = () => {
 
         let defaultClass: string = "";
         if (props.dismisable) {
             defaultClass = " alert-dismissible ";
-        }       
+        }
 
         if (props.sticky) {
             const position = `${props.position === "top" ? " position-absolute top-0 start-0 fullWidth z-index" : " position-absolute bottom-0 start-0 fullWidth z-index"}`;
             defaultClass = defaultClass + defaultClass + position;
         }
-                
+
         if (props.border === "none") {
             defaultClass = defaultClass + " shadow ";
         }
-          
+
         if (props.border === "single") {
             defaultClass = defaultClass + " border-" + (props.type === "info" ? "dark" : colorType);
         }
-  
+
 
         if (props.border === "left border") {
-            defaultClass = defaultClass + "border-"+ (props.type === "info" ? "dark" : colorType) + " alert-left-border " + colorType;
+            defaultClass = defaultClass + "border-" + (props.type === "info" ? "dark" : colorType) + " alert-left-border " + colorType;
         }
-  
+
         const sizeClass = `${props.size === "small" ? " alert-sm" : props.size === "large" ? " alert-lg" : " alert-md"}`;
         defaultClass = defaultClass + sizeClass;
         return defaultClass;
     };
-   
+
     return (
         <>
             <div
                 className={
                     "alert alert-" + colorType +
-                  " justify-content-between align-items-top z-0 position-relative d-lg-flex d-md-flex d-sm-block px-3 " + classes()}
+                    " justify-content-between align-items-top z-0 position-relative d-lg-flex d-md-flex d-sm-block px-3 " + classes()}
                 role="alert"
             >
                 {props.displayType == "singleline" && (
@@ -97,22 +97,22 @@ const RdsAlert = (props: RdsAlertProps) => {
                                     stroke={props.iconStroke}
                                     height={props.iconHeight}
                                     width={props.iconWidth}
-                                    colorVariant= "{colorType}"
+                                    colorVariant="{colorType}"
                                     classes="me-2"
                                 />
                             )}
-      
+
                             <div>
                                 {props.title && <strong>{props.title}</strong>}
                                 <span className="ps-2"> {props.message} </span>
                                 {props.showlink === true && (
                                     <a className=" text-decoration-underline ms-2" href="#">
-                                      Link
+                                        Link
                                     </a>
                                 )}
                             </div>
                         </span>
-                  
+
                         <span className="d-flex me-3">
                             <div className="d-flex gap-2 alertBtns pe-1 ">
                                 {props.showbutton === true && (
@@ -125,7 +125,7 @@ const RdsAlert = (props: RdsAlertProps) => {
                                         label="Okay"
                                     />
                                 )}
-                               <div className={`d-flex align-items-center justify-content-end gap-2 alert-close alert-${props.size}`}>
+                                <div className={`d-flex align-items-center justify-content-end gap-2 alert-close alert-${props.size}`}>
                                     {props.dismisable === true && (
                                         <RdsIcon
                                             colorVariant="primary"
@@ -142,7 +142,7 @@ const RdsAlert = (props: RdsAlertProps) => {
                         </span>
                     </>
                 )}
-      
+
                 {props.displayType == "multiline" && (
                     <>
                         <span className="custom-alert-message wordbreak d-flex align-items-center flex-column flex-md-row">
@@ -165,7 +165,7 @@ const RdsAlert = (props: RdsAlertProps) => {
                                 {props.message}
                                 <p>{props.description}</p>
                                 {props.showlink === true && (
-                                    <a className="text-decoration-underline d-block mt-2 mt-md-4" href="#"  id="alertlinks">
+                                    <a className="text-decoration-underline d-block mt-2 mt-md-4" href="#" id="alertlinks">
                                         Link
                                     </a>
                                 )}
