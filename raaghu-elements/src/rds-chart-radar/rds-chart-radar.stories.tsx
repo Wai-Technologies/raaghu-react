2bd0113ca2988dea7afe65e7eae1e2c019676625
VenuKG <venu.ghodake@waiin.com>
2024-12-17 09:44:05 +0530

    commit changes for bug-1264-design system-radar chart inside in chart

--- raaghu-elements/src/rds-chart-radar/rds-chart-radar.stories.tsx
+++ raaghu-elements/src/rds-chart-radar/rds-chart-radar.stories.tsx
@@ -2,7 +2,7 @@ import RdsRadarChart from "./rds-chart-radar";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta = {
-    title: 'Elements/Radar Chart',
+    title: 'Elements/Charts/Radar Chart',
     component: RdsRadarChart,
     parameters: {
         layout: 'padded',
