a6639a9b5259eb937bbe116d00d6cc627a088a70
atharvamachave28 <atharva.machave@waiin.com>
2025-01-03 16:55:36 +0530

    improved code commit

--- raaghu-elements/src/rds-color-picker/rds-color-picker.stories.tsx
+++ raaghu-elements/src/rds-color-picker/rds-color-picker.stories.tsx
@@ -15,44 +15,44 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj<typeof RdsColorPicker>;
 
-export const basic: Story = {
+export const ColorPicker: Story = {
     args: {
         value: "#e1e1e1",
         isDisabled: false,
-        label: "basic Color-Picker",
-        storyType: "basic"
+        label: "Selected Color",
+        //displayType: "basic"
     }
 } satisfies Story;
 
-export const hex: Story = {
-    args: {
-        value: "#e1e1e1",
-        isDisabled: false,
-        label: "Hex Color-Picker",
-        storyType: "hex"
-    }
-} satisfies Story;
+// export const hex: Story = {
+//     args: {
+//         value: "#e1e1e1",
+//         isDisabled: false,
+//         label: "Hex Color-Picker",
+//         //displayType: "hex"
+//     }
+// } satisfies Story;
 
-export const rgba: Story = {
-    args: {
-        value: "#FF5733", // Red color as default
-        isDisabled: true,  // Disabled state
-        label: "rgba Color Picker",
-        storyType: "rgba"
-    },
-} satisfies Story;
-export const hsl: Story = {
-    args: {
-        value: "#FF5733", // Red color as default
-        isDisabled: true,  // Disabled state
-        label: "hsl Color Picker",
-        storyType: "hsl"
-    },
-} satisfies Story;
+// export const rgba: Story = {
+//     args: {
+//         value: "#FF5733", // Red color as default
+//         isDisabled: true,  // Disabled state
+//         label: "rgba Color Picker",
+//         //displayType: "rgba"
+//     },
+// } satisfies Story;
+// export const hsl: Story = {
+//     args: {
+//         value: "#FF5733", // Red color as default
+//         isDisabled: true,  // Disabled state
+//         label: "hsl Color Picker",
+//         //displayType: "hsl"
+//     },
+// } satisfies Story;
 
 
-basic.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
-hex.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
-rgba.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
-hsl.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
+ColorPicker.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
+// hex.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
+// rgba.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
+// hsl.parameters = { controls: { include: ['value', 'label', 'isDisabled'] } };
 
--- raaghu-elements/src/rds-color-picker/rds-color-picker.tsx
+++ raaghu-elements/src/rds-color-picker/rds-color-picker.tsx
@@ -1,12 +1,120 @@
+// import React, { useState, Fragment, useEffect } from "react";
+// import "./rds-color-picker.css";
+// import { SketchPicker } from "react-color";
+
+
+// export interface RdsColorPickerProps {
+//     value: string;
+//     isDisabled?: boolean;
+//     label: string;
+//     displayType?: "basic" | "hex" | "rgba" | "hsl";
+// }
+
+// const RdsColorPicker = (props: RdsColorPickerProps) => {
+//     const Value = props.value;
+
+//     let [Color, setColor] = useState(Value);
+
+//     useEffect(() => {
+//         setColor(Value);
+//     }, [Value]);
+
+//     const HandlerChangecolor = (color: any) => {
+//         // color.hex will contain the selected hex color
+//         setColor(color.hex);
+//     };
+
+//     // Array of predefined colors for the swatcher (Optional)
+//     const colorSwatches = [
+//         "#FFC300", "#FF4F00", "#EA00FA", "#1708FF", "#00F5FF"
+//     ];
+
+//     const handleSwatchClick = (swatchColor: string) => {
+//         setColor(swatchColor);
+//     };
+
+//     return (
+//         <Fragment>
+//             {props.displayType == "basic" && (
+//                 <>
+//                     <div className="m-2">
+//                         {/* Color Swatcher Section */}
+//                         <div className="mt-2">
+//                             <label className="text-center">Choose from color swatches</label>
+//                             <div className="d-flex mt-2">
+//                                 {colorSwatches.map((color, index) => (
+//                                     <div
+//                                         key={index}
+//                                         onClick={() => handleSwatchClick(color)}
+//                                         style={{
+//                                             backgroundColor: color,
+//                                             width: "30px",
+//                                             height: "30px",
+//                                             borderRadius: "50%",
+//                                             cursor: "pointer",
+//                                             marginRight: "10px",
+//                                         }}
+//                                     ></div>
+//                                 ))}
+//                             </div>
+//                         </div>
+//                         <div className="text-start" >
+//                             <label className="mt-4" >{props.label}</label>
+//                         </div>
+
+//                         {/* Color Picker Section */}
+//                         <div className="align-items-center col-md-3 col-xl-2 col-6 d-flex mt-1 p-2">
+//                             <span className="me-3">
+//                                 <input
+//                                     type="color"
+//                                     className="form-control form-control-color colorPick"
+//                                     value={Color}
+//                                     id="color"
+//                                     disabled={props.isDisabled}
+//                                     onChange={HandlerChangecolor}
+//                                     title="Choose your color"
+//                                     data-testId="colorPicker"
+//                                 />
+//                             </span>
+//                         </div>
+//                     </div>
+//                 </>
+//             )}
+
+//             {props.displayType === "hex" && (
+//                 <>
+//                     <div className="m-2">
+//                         <div className="text-start" >
+//                             {/* Label for the color picker */}
+//                             <label>{props.label}</label>
+//                         </div>
+
+//                         <div className="align-items-center col-md-3 col-xl-2 col-6 d-flex mt-1 p-2">
+//                             <span className="me-3">
+//                                 {/* SketchPicker */}
+//                                 <SketchPicker
+//                                     color={Color}
+//                                     onChange={HandlerChangecolor}
+//                                     disabled={props.isDisabled}
+//                                 />
+//                             </span>
+//                         </div>
+//                     </div>
+//                 </>
+//             )}
+//         </Fragment>
+//     );
+// };
+
+// export default RdsColorPicker;
+
 import React, { useState, Fragment, useEffect } from "react";
 import "./rds-color-picker.css";
-import { SketchPicker } from "react-color";
 
 export interface RdsColorPickerProps {
     value: string;
     isDisabled?: boolean;
     label: string;
-    storyType: "basic" | "hex" | "rgba" | "hsl";
 }
 
 const RdsColorPicker = (props: RdsColorPickerProps) => {
@@ -17,73 +125,92 @@ const RdsColorPicker = (props: RdsColorPickerProps) => {
     useEffect(() => {
         setColor(Value);
     }, [Value]);
-
-    const HandlerChangecolor = (color: any) => {
-        // color.hex will contain the selected hex color
-        setColor(color.hex);
+    const HandlerChangecolor = (e: any) => {
+        const { value } = e.target;
+        console.log(value);
+        return (
+            setColor(Color = value));
     };
+
+    // Array of predefined colors for the swatcher (Optional)
+    const colorSwatches = [
+        "#FFC300", "#FF4F00", "#EA00FA", "#1708FF", "#00F5FF"
+    ];
 
     const handleSwatchClick = (swatchColor: string) => {
         setColor(swatchColor);
     };
 
+    const stri = Color;
+    const isValidHexaCode = () => {
+        if (stri[0] != "#")
+            return false;
+
+        if (!(stri.length == 4 || stri.length == 7))
+            return false;
+
+        for (let i = 1; i < stri.length; i++)
+            if (!((stri[i].charCodeAt(0) <= "0".charCodeAt(0) && stri[i].charCodeAt(0) <= 9)
+                || (stri[i].charCodeAt(0) >= "a".charCodeAt(0) && stri[i].charCodeAt(0) <= "f".charCodeAt(0))
+                || (stri[i].charCodeAt(0) >= "A".charCodeAt(0) || stri[i].charCodeAt(0) <= "F".charCodeAt(0))))
+                return false;
+
+        return true;
+    };
+    // Driver Code
+    if (isValidHexaCode() === true) {
+        Color = stri;
+    }
+    else {
+        Color = "#000000";
+    }
+
     return (
         <Fragment>
+            <div className="m-2">
+                {/* <div>
+                    <label>{props.label}</label>
+                </div> */}
 
-            {props.storyType == "basic" && (
-                <>
-                    <div className="m-2">
-                        <div className="text-start" >
-                            {/* Label for the color picker */}
-                            <label>{props.label}</label>
-                        </div>
-
-                        <div className="align-items-center col-md-3 col-xl-2 col-6 d-flex mt-1 p-2">
-                            <span className="me-3">
-
-                                <input
-                                    type="color"
-                                    className="form-control form-control-color colorPick"
-                                    value={Color}
-                                    id="color"
-                                    disabled={props.isDisabled}
-                                    onChange={HandlerChangecolor}
-                                    title="Choose your color"
-                                    data-testId="colorPicker"
-                                />
-
-                            </span>
-                        </div>
+                {/* Color Swatcher Section */}
+                <div className="mt-2">
+                    <label className="text-center">Choose from color swatches</label>
+                    <div className="d-flex mt-2">
+                        {colorSwatches.map((color, index) => (
+                            <div
+                                key={index}
+                                onClick={() => handleSwatchClick(color)}
+                                style={{
+                                    backgroundColor: color,
+                                    width: "30px",
+                                    height: "30px",
+                                    borderRadius: "50%",
+                                    cursor: "pointer",
+                                    marginRight: "10px",
+                                }}
+                            ></div>
+                        ))}
                     </div>
-                </>
-            )}
+                </div>
 
-            {props.storyType === "hex" && (
-                <>
-                    <div className="m-2">
-                        <div className="text-start" >
-                            {/* Label for the color picker */}
-                            <label>{props.label}</label>
-                        </div>
+                <div className=" align-items-center border col-md-3 col-xl-2 col-6 d-flex mt-4 p-2" >
+                    <span className="me-3">
+                        <input
+                            type="color"
+                            className="form-control form-control-color colorPick"
+                            value={Color}
+                            id="color"
+                            disabled={props.isDisabled}
+                            onChange={HandlerChangecolor}
+                            title="Choose your color"
+                            data-testId="colorPicker"
+                        />
+                    </span>
+                    <span>{Color}</span>
 
-                        <div className="align-items-center col-md-3 col-xl-2 col-6 d-flex mt-1 p-2">
-                            <span className="me-3">
-                                {/* SketchPicker */}
-                                <SketchPicker
-                                    color={Color}
-                                    onChange={HandlerChangecolor}
-                                    disabled={props.isDisabled}
-                                />
-                            </span>
-                        </div>
-                    </div>
-                </>
-            )}
-
+                </div>
+            </div>
         </Fragment>
     );
 };
-
 export default RdsColorPicker;
-
-
