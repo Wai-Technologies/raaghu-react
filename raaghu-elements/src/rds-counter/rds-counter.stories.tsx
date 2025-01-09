af62ce7994d177e9d11f2bfd602d3bb0e22dc156
Sohesh Gandhe <sohesh.gandhe@waiin.com>
2025-01-09 11:57:51 +0530

    Added default, side and bottom counter following design

--- raaghu-elements/src/rds-counter/rds-counter.stories.tsx
+++ raaghu-elements/src/rds-counter/rds-counter.stories.tsx
@@ -22,10 +22,14 @@ const meta: Meta = {
             ],
             control: { type: "select" },
         },
-        position: {
-            options: ["top", "bottom", "left", "right"],
+        type: {
+            options: ["default", "side", "bottom"],
             control: { type: "radio" },
         },
+        // position: {
+        //     options: ["top", "bottom", "left", "right"],
+        //     control: { type: "radio" },
+        // },
     },
 } satisfies Meta<typeof RdsCounter>;
 
@@ -34,13 +38,60 @@ type Story = StoryObj<typeof RdsCounter>;
 
 export const Counter: Story = {
     args: {
-        // counterValue: 0,
+        counterValue: 0,
         min: 0,
         max: 50,
-        width: 135,
+        width: 400,
         colorVariant: "primary",
-        position: "top",
+        type: "default", // Default button placement
         label: "Counter",
-    }
-} satisfies Story;
-Counter.parameters = { controls: { include: ['min', 'max', 'width', 'colorVariant', 'position', 'label'] } };
+    },
+};
+Counter.parameters = { controls: { include: ['min', 'max', 'width', 'colorVariant', 'type', 'label'] } };
+
+// import RdsCounter from "./rds-counter";
+// import { Meta, StoryObj } from "@storybook/react";
+
+// const meta: Meta = {
+//     title: 'Elements/Counter',
+//     component: RdsCounter,
+//     parameters: {
+//         layout: 'padded',
+//     },
+//     tags: ['autodocs'],
+//     argTypes: {
+//         colorVariant: {
+//             options: [
+//                 "primary",
+//                 "secondary",
+//                 "success",
+//                 "info",
+//                 "warning",
+//                 "danger",
+//                 "dark",
+//                 "light",
+//             ],
+//             control: { type: "select" },
+//         },
+//         position: {
+//             options: ["top", "bottom", "left", "right"],
+//             control: { type: "radio" },
+//         },
+//     },
+// } satisfies Meta<typeof RdsCounter>;
+
+// export default meta;
+// type Story = StoryObj<typeof RdsCounter>;
+
+// export const Counter: Story = {
+//     args: {
+//         // counterValue: 0,
+//         min: 0,
+//         max: 50,
+//         width: 135,
+//         colorVariant: "primary",
+//         position: "top",
+//         label: "Counter",
+//     }
+// } satisfies Story;
+// Counter.parameters = { controls: { include: ['min', 'max', 'width', 'colorVariant', 'position', 'label'] } };
