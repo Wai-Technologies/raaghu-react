76a63bc7cd4f2b2766e67a5db9b7312c2c07dc21
Nitin Sonawane <nitin.sonawane@waiin.com>
2025-02-12 09:54:22 +0530

    Commit Changes for task 1459

--- raaghu-elements/src/rds-video-player/rds-video-player.tsx
+++ raaghu-elements/src/rds-video-player/rds-video-player.tsx
@@ -7,17 +7,26 @@ export interface RdsVideoPlayerProps {
     autoplay: boolean;
     muted: boolean;
     videoLink: string;
+    type?: "Default" | "YouTube" | "Vimeo";
 }
 
-const RdsVideoPlayer = (props: RdsVideoPlayerProps) => {
+const RdsVideoPlayer = ({ width, height, autoplay, muted, videoLink, type }: RdsVideoPlayerProps) => {
+    let formattedLink = videoLink;
+
+    if (type === "YouTube" && !videoLink.includes("youtube.com") && !videoLink.includes("youtu.be")) {
+        formattedLink = "https://youtu.be/7sDY4m8KNLc"; // Default YouTube video
+    } else if (type === "Vimeo" && !videoLink.includes("vimeo.com")) {
+        formattedLink = "https://vimeo.com/420192272"; // Default Vimeo video
+    }
+
     return (
         <div>
             <ReactPlayer
-                url={props.videoLink}
-                width={props.width}
-                height={props.height}
-                playing={props.autoplay}
-                muted={props.muted}
+                url={formattedLink}
+                width={width}
+                height={height}
+                playing={autoplay}
+                muted={muted}
                 controls={true}
             />
         </div>
