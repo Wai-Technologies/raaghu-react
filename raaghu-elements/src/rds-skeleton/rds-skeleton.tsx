550e91de468124e09b8452a25f4674952632fafd
Nitin Sonawane <nitin.sonawane@waiin.com>
2025-02-15 12:49:44 +0530

    Commit Changes for task 1457

--- raaghu-elements/src/rds-skeleton/rds-skeleton.tsx
+++ raaghu-elements/src/rds-skeleton/rds-skeleton.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import "./rds-skeleton.css";
 
 interface RdsSkeletonProps {
-  shape: "rectangle" | "circle";
-  isAnimated: boolean;
+  shape?: "rectangle" | "circle";
+  isAnimated?: boolean;
   width?: string;
   height?: string;
+  state?: 1 | 2 | 3; 
 }
 
 const RdsSkeleton = (props: RdsSkeletonProps) => {
-  const { shape, isAnimated, width, height } = props;
+  const { shape, isAnimated, width, height, state } = props;
   const sizeStyle = {
-    width: width ?? "200px",
-    height: shape === "circle" ? width : height ?? "100px",
+    width: width ?? "150px",
+    height: shape === "circle" ? width ?? "150px" : height ?? "100px",
   };
 
   return (
     <div
-      className={`rds-skeleton ${shape} ${
+      className={`rds-skeleton ${shape} state-${state} ${
         isAnimated ? "is-animated" : "static"
       }`}
       style={sizeStyle}
