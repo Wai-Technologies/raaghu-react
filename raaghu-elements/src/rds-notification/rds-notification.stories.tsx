e82eb042a48e42f8ee3c5e8aa4d79b4bc11e65e8
Sachin Ghorpade <sachin.ghorpade@waiin.com>
2025-02-20 16:35:24 +0530

    Committed Changes on Notification Enhancement as per Figma Design Kit

--- raaghu-elements/src/rds-notification/rds-notification.stories.tsx
+++ raaghu-elements/src/rds-notification/rds-notification.stories.tsx
@@ -1,28 +1,26 @@
 import { Meta, StoryObj } from "@storybook/react";
 import RdsNotification from "./rds-notification";
 
-
 const meta: Meta = {
     title: "Elements/Notification",
     component: RdsNotification,
 
     parameters: {
         layout: 'padded',
-      },
-      tags: ['autodocs'],
-      
+    },
+    tags: ['autodocs'],
+
     argTypes: {
-        colorVariant: {
-            options: [
-                "primary",
-                "secondary",
-                "success",
-                "info",
-                "warning",
-                "danger",
-                "dark",
-                "light",
-            ],
+        layout: {
+            options: ["vertical", "horizontal"],
+            control: { type: "select" },
+        },
+        style: {
+            options: ["default", "avatar", "icon", "image"],
+            control: { type: "select" },
+        },
+        type: {
+            options: ["error", "info", "success", "warning"],
             control: { type: "select" },
         },
     }
@@ -33,48 +31,105 @@ type Story = StoryObj<typeof RdsNotification>;
 
 export const Default: Story = {
     args: {
-        colorVariant: "light",
+        layout: "vertical",
+        style: "default",
+        type: "info",
         notifications: [
             {
-                status: "success", title: "Tenant added",
+                status: "success",
+                title: "Tenant added",
                 urlTitle: "hello",
-                // url:" " ,
                 time: "a month ago",
                 state: 1,
                 userNotificationId: 0,
                 selected: false
-            },
-
-            {
-                status: "error", title: "Tenant deleted",
-                urlTitle: "hello",
-                time: "a month ago",
-                state: 1,
-                userNotificationId: 1,
-                selected: false
-            },
-
-            {
-                status: "warn", title: "Tenant added  warn",
-                urlTitle: "hello",
-                time: "a month ago",
-                state: 1,
-                userNotificationId: 2,
-                selected: false
-            },
-
-            {
-                status: "info", title: "Tenant deleted info",
-                urlTitle: "hello",
-                time: "a month ago",
-                state: 1,
-                userNotificationId: 3,
-                selected: false
             }
-
         ]
     }
 }
-Default.parameters = { controls: { include: ['colorVariant', 'notifications'] } };
+Default.parameters = { controls: { include: ['layout', 'style', 'type'] } };
+
+
+
+
+
+// import { Meta, StoryObj } from "@storybook/react";
+// import RdsNotification from "./rds-notification";
+
+
+// const meta: Meta = {
+//     title: "Elements/Notification",
+//     component: RdsNotification,
+
+//     parameters: {
+//         layout: 'padded',
+//       },
+//       tags: ['autodocs'],
+      
+//     argTypes: {
+//         colorVariant: {
+//             options: [
+//                 "primary",
+//                 "secondary",
+//                 "success",
+//                 "info",
+//                 "warning",
+//                 "danger",
+//                 "dark",
+//                 "light",
+//             ],
+//             control: { type: "select" },
+//         },
+//     }
+// } satisfies Meta<typeof RdsNotification>;
+
+// export default meta;
+// type Story = StoryObj<typeof RdsNotification>;
+
+// export const Default: Story = {
+//     args: {
+//         colorVariant: "light",
+//         notifications: [
+//             {
+//                 status: "success", title: "Tenant added",
+//                 urlTitle: "hello",
+//                 // url:" " ,
+//                 time: "a month ago",
+//                 state: 1,
+//                 userNotificationId: 0,
+//                 selected: false
+//             },
+
+//             {
+//                 status: "error", title: "Tenant deleted",
+//                 urlTitle: "hello",
+//                 time: "a month ago",
+//                 state: 1,
+//                 userNotificationId: 1,
+//                 selected: false
+//             },
+
+//             {
+//                 status: "warn", title: "Tenant added  warn",
+//                 urlTitle: "hello",
+//                 time: "a month ago",
+//                 state: 1,
+//                 userNotificationId: 2,
+//                 selected: false
+//             },
+
+//             {
+//                 status: "info", title: "Tenant deleted info",
+//                 urlTitle: "hello",
+//                 time: "a month ago",
+//                 state: 1,
+//                 userNotificationId: 3,
+//                 selected: false
+//             }
+
+//         ]
+//     }
+// }
+// Default.parameters = { controls: { include: ['colorVariant', 'notifications'] } };
 
 
