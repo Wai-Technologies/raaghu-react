ef2bd2903998f884e6edebb4ff46ae72a88b1ff6
nknikhilkasar <nikhil.kasar@waiin.com>
2025-02-21 19:52:08 +0530

    Add RdsChat component and integrate with Storybook; enhance RdsFabMenu with click outside functionality

---
+++ raaghu-elements/src/rds-chat/rds-chat.stories.tsx
@@ -0,0 +1,38 @@
+import RdsChat from "./rds-chat";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+    title: "Elements/Chat",
+    component: RdsChat,
+    parameters: {
+        layout: "padded",
+    },
+    tags: ["autodocs"]
+} satisfies Meta<typeof RdsChat>;
+
+export default meta;
+type Story = StoryObj<typeof RdsChat>;
+
+export const Chat: Story = {
+    args: {
+        isChatScreenEnabled: true,
+        userData: [{firstName: 'Wai', lastName: 'Technologies', activeDotButton: true, status: 'Typing....', size: "medium", colorVariant:"primary", time: '13.00', profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJxA5cTf-5dh5Eusm0puHbvAhOrCRPtckzjA&usqp=CAU', messageStatus: 'sent', comments: [{
+            firstName: "Alice",
+            lastName: "Smith",
+            comment: "Hi, How are you?",
+        },{
+            firstName: "Alice",
+            lastName: "Smith",
+            comment: "This is sample message.",
+        }]},
+            {firstName: 'Nikhil', lastName: 'Kasar', activeDotButton: false, status: 'Developer', size: "medium", colorVariant:"primary", time: '13.00', profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0m5Cy4lXCbuyG54L0vuo3i5-ALavHe9KmhWA_wDM&s', messageStatus: '', comments: []},
+            {firstName: 'Supriya', lastName: 'Tambe', activeDotButton: false, status: 'Company Owner', size: "medium", colorVariant:"primary", time: '13.00', profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0m5Cy4lXCbuyG54L0vuo3i5-ALavHe9KmhWA_wDM&s', messageStatus: '', comments: []}
+        ],
+        currentUserCommentBgColor: '#7825E9',
+        currentUserCommentTextColor: '#FEF7FF', 
+        otherUserCommentBgColor: '#D6D6D6',
+        OtherUserCommentTextColor: '#202020'
+    }
+} satisfies Story;
+
+Chat.parameters = { controls: { include: ["isChatScreenEnabled"] } };
