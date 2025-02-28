e3c773598a0b89089f9f4f1c0e777daa5a940ccd
Dipak7028 <dipak.charhate@waiin.com>
2025-02-28 19:05:00 +0530

    Resolved

--- raaghu-elements/src/rds-dropdown/rds-dropdown.tsx
+++ raaghu-elements/src/rds-dropdown/rds-dropdown.tsx
@@ -167,7 +167,7 @@ const RdsDropdown = (props: RdsDropdownProps) => {
   return (
     <>
       {props.displayType === 'dropdown' && (
-        <div className="dropdown">
+        <div className={`dropdown ${(props.style === 'secondary' || props.style === 'outline') ? 'rectangle' : ''} `  }>
           {props.tooltip ? (
             <Tooltip text={props.tooltipTitle} place={props.tooltipPlacement}>
               {renderButton()}
