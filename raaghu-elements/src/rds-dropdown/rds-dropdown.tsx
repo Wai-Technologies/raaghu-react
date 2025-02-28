b6211d3affff1ce3f22e321bcf8009ba0159a48a
Dipak7028 <dipak.charhate@waiin.com>
2025-02-28 19:39:18 +0530

    Update rds-dropdown.tsx

--- raaghu-elements/src/rds-dropdown/rds-dropdown.tsx
+++ raaghu-elements/src/rds-dropdown/rds-dropdown.tsx
@@ -167,7 +167,7 @@ const RdsDropdown = (props: RdsDropdownProps) => {
   return (
     <>
       {props.displayType === 'dropdown' && (
-        <div className={`dropdown ${(props.style === 'secondary' || props.style === 'outline') ? 'rectangle' : ''}  ${(props.shape === 'pill' || props.style === 'outline' ) ? 'pill' : ''} `  }>
+        <div className={`dropdown ${(props.style === 'secondary' || props.style === 'outline') ? 'rectangle' : ''}  ${(props.shape === 'pill') ? 'pill' : ''} `  }>
           {props.tooltip ? (
             <Tooltip text={props.tooltipTitle} place={props.tooltipPlacement}>
               {renderButton()}
