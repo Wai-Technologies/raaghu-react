781ec58a0fc77d4441be0995028db14c15f0f867
nknikhilkasar <nikhil.kasar@waiin.com>
2025-03-18 19:40:19 +0530

    Add RdsPlanSwitcher component to subscription plan for plan selection

--- raaghu-components/src/rds-comp-subscription-plan/rds-comp-subscription-plan.tsx
+++ raaghu-components/src/rds-comp-subscription-plan/rds-comp-subscription-plan.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./rds-comp-subscription-plan.css";
-import { RdsIcon } from "../rds-elements";
+import { RdsIcon, RdsPlanSwitcher } from "../rds-elements";
 
-export interface RdsSubscriptionPlanProps { 
+export interface RdsSubscriptionPlanProps {
   planAmount?: string;
 }
 
@@ -20,74 +20,58 @@ const RdsCompSubscriptionPlan = (props: RdsSubscriptionPlanProps) => {
   return (<>
     <div className="subscriptionContainer">
       <div className="closeButton" ><RdsIcon
-  colorVariant="dark"
-  height="16px"
-  isCursorPointer
-  name="close"
-  stroke
-  width="16px"
-  onClick={closeModal}
-/></div>
+        colorVariant="dark"
+        height="16px"
+        isCursorPointer
+        name="close"
+        stroke
+        width="16px"
+        onClick={closeModal}
+      /></div>
       <div className="backGifContainer">
         <img src={"./assets/backGif.gif"} alt="ssj" className="backGif" />
         <img src={"./assets/Panel-0421.png"} alt="ssj" className="panelImage" />
       </div>
       <div>
-      <div className="centerText">
-        <div className="upgradeText">Upgrade to<img src={"./assets/AIPunditColored.png"} alt="ssj" />
-        <span className="aiPunditChat">AI Pundit Chat</span><span className="proText">Pro</span></div>
-        <div className="currentPlanText">You’re currently on the free plan</div>
-        <div className="planToggle">
-          <div 
-            onClick={() => setIsPlanFree(true)} 
-            className={`planOption equalWidth ${isPlanFree ? "multicolorBorder" : ""}`} 
-            id="1"
-          >
-            Free
-          </div>
-          <div 
-            onClick={() => setIsPlanFree(false)} 
-            className={`planOption equalWidth ${!isPlanFree ? "premiumSelected" : ""}`} 
-            id="2"
-          >
-            <img 
-              src={isPlanFree ? "./assets/crown.png" : "./assets/crownwhite.png"} 
-              alt="ssj" 
-              className="crownImage" 
-            />
-            Premium
+        <div className="centerText">
+          <div className="upgradeText">Upgrade to<img src={"./assets/AIPunditColored.png"} alt="ssj" />
+            <span className="aiPunditChat">AI Pundit Chat</span><span className="proText">Pro</span></div>
+          <div className="currentPlanText">You’re currently on the free plan</div>
+          <RdsPlanSwitcher
+            button1Text="Free"
+            button2Text="Premium"
+            setIsPlanFree={setIsPlanFree}
+          />
+        </div>
+        <div>
+          <div className="planDetails">
+            {isPlanFree ? <div >
+              <div className="freeText">Free</div>
+              <div className="perfectForIndividuals">Perfect for individuals exploring our platform.</div>
+              <div className="priceText">$0<span>/month</span></div>
+              <hr />
+              <div className="forIndividualsText">For Individuals:</div>
+              <div className="forProUsersText">For pro users:</div>
+              <div className="accessChat"><img src="./assets/check.png" />Access to chat.raaghu.ai</div>
+              <div className="accessChat"><img src="./assets/check.png" />Up to 5 credits daily</div>
+              <div className="accessChat"><img src="./assets/check.png" />Share chat URL with anyone</div>
+            </div> : <div>
+              <div className="freeText">Premium</div>
+              <div className="perfectForIndividuals">Take your projects to the next level with pro features.</div>
+              <div className="priceText">${subscriptionPlanAmount}<span>/month</span></div>
+              <hr />
+              <div className="forIndividualsText">What’s included</div>
+              <div className="forProUsersText">For pro users:</div>
+              <div className="accessChat"><img src="./assets/check.png" />Everything in free</div>
+              <div className="accessChat"><img src="./assets/check.png" />Higher messaging limits</div>
+              <div className="accessChat"><img src="./assets/check.png" />Custom domains on integration option</div>
+              <div className="accessChat"><img src="./assets/check.png" />Unlimited Projects</div>
+              <div className="accessChat"><img src="./assets/check.png" />Import from Figma</div>
+              <div className="accessChat"><img src="./assets/check.png" />Integrate ABP</div>
+              <div className="accessChat"><img src="./assets/check.png" />Integrate ASP.NET Zero</div>
+            </div>}
           </div>
         </div>
-      </div>
-      <div>
-        <div className="planDetails">
-          {isPlanFree ? <div >
-          <div className="freeText">Free</div>
-          <div className="perfectForIndividuals">Perfect for individuals exploring our platform.</div>
-          <div className="priceText">$0<span>/month</span></div>
-          <hr />
-          <div className="forIndividualsText">For Individuals:</div>
-          <div className="forProUsersText">For pro users:</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Access to chat.raaghu.ai</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Up to 5 credits daily</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Share chat URL with anyone</div>
-          </div> : <div>
-          <div className="freeText">Premium</div>
-          <div className="perfectForIndividuals">Take your projects to the next level with pro features.</div>
-          <div className="priceText">${subscriptionPlanAmount}<span>/month</span></div>
-          <hr />
-          <div className="forIndividualsText">What’s included</div>
-          <div className="forProUsersText">For pro users:</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Everything in free</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Higher messaging limits</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Custom domains on integration option</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Unlimited Projects</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Import from Figma</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Integrate ABP</div>
-          <div className="accessChat"><img src="./assets/check.png"/>Integrate ASP.NET Zero</div>
-          </div>}
-        </div>
-      </div>
       </div>
     </div>
   </>);
