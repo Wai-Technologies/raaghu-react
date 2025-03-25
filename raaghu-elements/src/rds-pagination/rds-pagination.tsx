7ca327c8e2c09831c9e41c7909c993a8b77924f8
supriyartambewaiin <supriya.tambe@waiin.com>
2025-03-25 17:37:32 +0530

    pagination alignment

--- raaghu-elements/src/rds-pagination/rds-pagination.tsx
+++ raaghu-elements/src/rds-pagination/rds-pagination.tsx
@@ -231,7 +231,7 @@ const RdsPagination = (props: RdsPaginationProps) => {
 
         {paginType === "default" && (props.style!="Style5" && props.style!="Style6" && props.style !="Style8" && props.style!="Style9" && props.style!="Style10" && props.style!="Style11") && (
           <nav aria-label="page navigation" className={"d-flex align-items-center" + `${size}` +`${align} ${props.style=='Style3' ?'bg-white ':''} ${props.style=='Style7' ?'bg-white rounded-5 ':''}`}>
-            <ul className={"pagination rounded align-items-center mb-0" + `${size}` + `${align} `}>
+            <ul className={"pagination rounded align-items-center mb-0 mt-2" + `${size}` + `${align} `}>
               {/* Previous Page Button */}
 
               {(
