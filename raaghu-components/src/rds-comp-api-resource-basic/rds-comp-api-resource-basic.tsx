e8ffb8e82ab975fff4bca5f98d6c77aa987701ee
Dipak7028 <dipak.charhate@waiin.com>
2025-04-05 13:00:43 +0530

    Update rds-comp-api-resource-basic.tsx

--- raaghu-components/src/rds-comp-api-resource-basic/rds-comp-api-resource-basic.tsx
+++ raaghu-components/src/rds-comp-api-resource-basic/rds-comp-api-resource-basic.tsx
@@ -47,7 +47,7 @@ const isFormValid = isNameValid(formData?.name);
       <div>
         <form>
           <div className="custom-content-scroll">
-            <div className="row mt-3 mb-4">
+            <div className="row">
               <div className="col-6">
                 <RdsInput
                   required={true}
@@ -80,7 +80,7 @@ const isFormValid = isNameValid(formData?.name);
                 ></RdsInput>
               </div>
             </div>
-            <div className=" mb-4">
+            <div className="">
               <RdsTextArea
                 label="Description"
                 placeholder="Description"
