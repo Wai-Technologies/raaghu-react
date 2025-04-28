5d220bd2c869036d4a0edbd50da14e5282f6f8a0
Piyush-Joshi-WaiTech <piyush.joshi@waiin.com>
2025-04-28 15:11:31 +0530

    commit for implement tailwindcss

---
+++ postcss.config.ts
@@ -0,0 +1,14 @@
+// module.exports = {
+//     plugins: {
+//         "@tailwindcss/postcss": {}, 
+//         autoprefixer: {},
+//     },
+// };
+// import { Config } from "tailwindcss";
+
+// export default {
+//     plugins: {
+//         tailwindcss: {},
+//         autoprefixer: {},
+//     },
+// } as Config;
---
+++ raaghu-pages/tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
