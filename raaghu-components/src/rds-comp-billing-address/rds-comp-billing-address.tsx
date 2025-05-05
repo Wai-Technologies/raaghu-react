3c0dd7914483e8256ff126044e09d64cfbf1acc5
Dipak7028 <dipak.charhate@waiin.com>
2025-05-05 12:15:50 +0530

    add

--- raaghu-components/src/rds-comp-billing-address/rds-comp-billing-address.tsx
+++ raaghu-components/src/rds-comp-billing-address/rds-comp-billing-address.tsx
@@ -358,7 +358,7 @@ function emitSaveData(event: any) {
                         </div>
                     </div>
                 </div>
-                <div className="d-flex flex-column-reverse ps-4 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3 p-4">
+                <div className="d-flex flex-column-reverse ps-4 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3  pb-2 px-4">
                        <RdsButton
                             class="me-2"
                             tooltipTitle={""}
