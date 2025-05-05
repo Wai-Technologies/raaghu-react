71058b9f9f662b3f9e55d8bd74000702b65b76a3
Dipak7028 <dipak.charhate@waiin.com>
2025-05-05 14:28:03 +0530

    Add Changes
    Add Changes for responsiveness

--- raaghu-components/src/rds-comp-email/rds-comp-email.tsx
+++ raaghu-components/src/rds-comp-email/rds-comp-email.tsx
@@ -249,7 +249,7 @@ const isFormValid = isSenderEmailValid(sendTestEmailData?.senderEmailAddress)&& 
                     {condition}
                 </form>
             </div>
-            <div className="d-flex flex-column-reverse ps-4 ms-2 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3 p-4">
+            <div className="d-flex flex-column-reverse ps-4 flex-lg-row flex-md-column-reverse flex-row flex-xl-row flex-xxl-row footer-buttons gap-2 mt-3 pb-3 px-4">
                 <div>
                     <RdsButton
                         label="Save"
