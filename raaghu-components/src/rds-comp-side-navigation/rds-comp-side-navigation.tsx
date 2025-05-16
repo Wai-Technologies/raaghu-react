36064ba935207bb528017d18718d2cf322b5da8a
rameshwarkamandar <155446782+rameshwarkamandar@users.noreply.github.com>
2025-05-16 08:48:22 +0530

    Resolve issue 956: Error is display

--- raaghu-components/src/rds-comp-side-navigation/rds-comp-side-navigation.tsx
+++ raaghu-components/src/rds-comp-side-navigation/rds-comp-side-navigation.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-import { RdsSideNav } from "../rds-elements";
+import React from 'react';
+import RdsSideNav from '../../../raaghu-elements/src/rds-side-nav/rds-side-nav';
 
-export interface sideNavItems {
+export interface SideNavItem {
   key: string;
   label: string;
   icon: string;
   path?: string;
-  children?: sideNavItems[];
+  children?: SideNavItem[];
 }
+
 export interface RdsCompSideNavigationProps {
-  sideNavItems: sideNavItems[];
+  sideNavItems: SideNavItem[];
   toggleTheme?: React.MouseEventHandler<HTMLInputElement>;
   toggleClass?: any;
-  collapse?: any;
+  collapse?: boolean;
   logo?: string;
   lockIconVisible?: boolean;
 }
 
 const RdsCompSideNavigation = (props: RdsCompSideNavigationProps) => {
-  return <RdsSideNav logo={props.logo} sideNavItems={props.sideNavItems} toggleTheme={props.toggleTheme} collapse={props.collapse} toggleClass={props.toggleClass} lockIconVisible></RdsSideNav>;
+  return (
+    <RdsSideNav
+      logo={props.logo}
+      sideNavItems={props.sideNavItems}
+      toggleTheme={props.toggleTheme}
+      collapse={props.collapse ?? false} // Default to false if not provided
+      toggleClass={props.toggleClass}
+      lockIconVisible={props.lockIconVisible ?? true} // Default to true if not provided
+    />
+  );
 };
 
 export default RdsCompSideNavigation;
