d66ddc2ec2e9f839a1fabfa03803e9e8214c27c7
AkshayPotphode <akshay.potphode@waiin.com>
2025-05-20 11:23:35 +0530

    Commit review changes

--- raaghu-pages/src/App.tsx
+++ raaghu-pages/src/App.tsx
@@ -7,18 +7,18 @@ function App() {
   return (
     <>
       <div className="mb-2 logo-row">
-        <a href="https://raaghu.io" target="_blank">
+        <a href="https://raaghu.io" target="_blank" rel="noopener noreferrer">
           <img src="https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/Raaghu%20Logo%20SD.svg" className="logo" alt="Raaghu Logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React Logo" />
         </a>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite Logo" />
         </a>
       </div>
-      <h1 className="mb-4 fw-bolder">Raaghu with React + Vite</h1>
-      <div className="">
+      <h1 className="mb-4 font-extrabold">Raaghu with React + Vite</h1>
+      <div>
         <p>
           Start creating pages by using Application Shells, Layouts, Component, Elements, Charts and more.
         </p>
