ff9cce7d8e209fd96c08b6dc1f06d608df93d56f
AkshayPotphode <akshay.potphode@waiin.com>
2025-05-23 14:58:58 +0530

    Commit for checkbox-parent-child component merged in checkbox-group

--- raaghu-elements/src/rds-checkbox-group/rds-checkbox-group.stories.tsx
+++ raaghu-elements/src/rds-checkbox-group/rds-checkbox-group.stories.tsx
@@ -56,6 +56,77 @@ export const MultiOptionCheckbox: Story = {
             },
         ],
         errorMessage: "Error Message",
+        multiOptionCheck:true,
     }
 }
 MultiOptionCheckbox.parameters = { controls: { include: ['state', 'label', 'isInline', 'isDisabled', 'isSwitch', 'itemList', 'errorMessage'] } };
+
+export const CheckboxParent: Story = {
+    args: {
+        userData: [
+            {
+                id: 1,
+                label: "Parent Checkbox 1",
+                isSelected: false,
+                isIntermediate: false,
+                disabled: false,
+                childList: [
+                    {
+                        id: 1,
+                        parent_id: 1,
+                        label: "Child Checkbox 1",
+                        isSelected: false,
+                        disabled: false,
+                    },
+                    {
+                        id: 2,
+                        parent_id: 1,
+                        label: "Child Checkbox 2",
+                        isSelected: false,
+                        disabled: false,
+                    },
+                    {
+                        id: 3,
+                        parent_id: 1,
+                        label: "Child Checkbox 3",
+                        isSelected: false,
+                        disabled: false,
+                    },
+                    {
+                        id: 4,
+                        parent_id: 1,
+                        label: "Child Checkbox 4",
+                        isSelected: false,
+                        disabled: false,
+                    },
+                ],
+            },
+            {
+                id: 2,
+                label: "Parent Checkbox 2",
+                isSelected: false,
+                isIntermediate: false,
+                isClosed: false,
+                disabled: false,
+                childList: [
+                    {
+                        id: 1,
+                        parent_id: 2,
+                        label: "Child Checkbox 1",
+                        isSelected: true,
+                        disabled: false,
+                    },
+                    {
+                        id: 2,
+                        parent_id: 2,
+                        label: "Child Checkbox 2",
+                        isSelected: true,
+                        disabled: false,
+                    },
+                ],
+            },
+        ],
+        multiOptionCheck: false,
+    }
+} satisfies Story;
+CheckboxParent.parameters = { controls : { include: ['userData'] } };
