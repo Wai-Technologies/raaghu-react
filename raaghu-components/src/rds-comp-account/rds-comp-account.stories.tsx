8284c794b3c80096bf50ceede51cf39c665fce12
AkshayPotphode <akshay.potphode@waiin.com>
2025-05-27 17:46:28 +0530

    Commit for Account External Provider and Linked Account merged into Account

--- raaghu-components/src/rds-comp-account/rds-comp-account.stories.tsx
+++ raaghu-components/src/rds-comp-account/rds-comp-account.stories.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import RdsCompAccount from './rds-comp-account';
 
-
-
 const meta: Meta = { 
     title: "Components/Account",
     component: RdsCompAccount,
@@ -28,10 +26,71 @@ type Story = StoryObj<typeof RdsCompAccount>;
 
 export const Default: Story = {
     args: {
-        
+        accountType: "resourceBasic",
     }
 } satisfies Story;
+Default.parameters = { controls: { include: ["accountGeneralSettings", "accountTwoFactorSettings", "accountCaptchaSettings", "developerModeSettings", "onSubmit", "reset", "onShow", "onShowDeveloperMode"] } };
 
+export const ExternalProvider: Story = {
+    args: {
+        accountType: "externalProvider",
+        accountExternalProvider: [
+            {
+                "name": "Google",
+                "enabled": false,
+                "properties": [
+                    {
+                        "name": "ClientId",
+                        "value": null
+                    }
+                ],
+                "secretProperties": [
+                    {
+                        "name": "ClientSecret",
+                        "value": null
+                    }
+                ]
+            },
+            {
+                "name": "Microsoft",
+                "enabled": false,
+                "properties": [
+                    {
+                        "name": "ClientId",
+                        "value": null
+                    }
+                ],
+                "secretProperties": [
+                    {
+                        "name": "ClientSecret",
+                        "value": null
+                    }
+                ]
+            },
+            {
+                "name": "Twitter",
+                "enabled": false,
+                "properties": [
+                    {
+                        "name": "ConsumerKey",
+                        "value": null
+                    }
+                ],
+                "secretProperties": [
+                    {
+                        "name": "ConsumerSecret",
+                        "value": null
+                    }
+                ]
+            }
+        ]
+    }
+} satisfies Story;
+ExternalProvider.parameters = { controls: { include: ["accountExternalProvider", "onSubmit"] } };
 
-
-
+export const Linked: Story = {
+    args: {
+      accountType: "linked",
+    }
+} satisfies Story;
+Linked.parameters = { controls: { include: ["linkedAccountData", "onSaveHandler", "reset"] } };
