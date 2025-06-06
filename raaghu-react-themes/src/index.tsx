8caf1bdaf449ab1a2822eced3e32de6c4002938e
supriyartambewaiin <supriya.tambe@waiin.com>
2025-06-06 11:25:25 +0530

    Remove commented-out ReactDOM rendering code from index.tsx

--- raaghu-react-themes/src/index.tsx
+++ raaghu-react-themes/src/index.tsx
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
