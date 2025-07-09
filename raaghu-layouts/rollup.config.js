fe8737d05bbdd2ce0950d10421f2daaa65e46da2
Kaushik Deepak Gokhale <kaushik.gokhale@waiin.com>
2025-07-09 16:14:32 +0530

    Updated Rollup Files for Tenser Imports

--- raaghu-layouts/rollup.config.js
+++ raaghu-layouts/rollup.config.js
@@ -2,8 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import terser from "@rollup/plugin-terser";
 import dts from "rollup-plugin-dts";
 import babel from "rollup-plugin-babel";
 import external from "rollup-plugin-peer-deps-external";
