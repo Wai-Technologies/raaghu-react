ddf6757f6ae532d7e0eb6fe7940efe17b2f4260f
Kaushik Deepak Gokhale <kaushik.gokhale@waiin.com>
2025-07-09 11:55:01 +0530

    Made changes for install-all solution to workspaces solution

--- utils/start-scripts.js
+++ utils/start-scripts.js
@@ -1,43 +1,45 @@
 
 // Imports
-const path = require('path');
-const child_process_1 = require("child_process");
+const { exec } = require('child_process');
 
-function startCmd(name, cmd) {
-    const process = child_process_1.exec(cmd);
+function startCmd(name, cmd, cwd = '.') {
+    const process = exec(cmd, { cwd });
     process.stdout.on('data', (chunk) => {
-        console.log(name, chunk);
+        console.log(`[${name}]`, chunk);
     });
     process.stderr.on('data', (chunk) => {
-        console.log(name, chunk, true);
+        console.error(`[${name}]`, chunk);
     });
-}
-
-function startApps(apps) {
-    for (const app of apps) {
-        const cmd = `cd .\\raaghu-mfe\\rds_pages\\${app} && npm run dev`;
-        console.log('DEVSVR: ', app);
-        startCmd(app, cmd);
-    }
+    process.on('close', (code) => {
+        console.log(`[${name}] Process exited with code ${code}`);
+    });
 }
 
 async function start() {
-
     const args = process.env.npm_config_projects ? process.env.npm_config_projects : undefined;
 
-    if (args != undefined && args != null) {
-        console.log('Starting...', args);
-        let projectToBuildArray = args.split(',');
-
-        startApps(projectToBuildArray);
-
+    if (args) {
+        console.log('Starting specific projects...', args);
+        const projects = args.split(',');
+        
+        for (const project of projects) {
+            const trimmedProject = project.trim();
+            if (['pages', 'storybook'].includes(trimmedProject)) {
+                if (trimmedProject === 'pages') {
+                    console.log('Starting pages...');
+                    startCmd('Pages', 'npm run dev', './raaghu-pages');
+                } else if (trimmedProject === 'storybook') {
+                    console.log('Starting storybook...');
+                    startCmd('Storybook', 'npm run storybook');
+                }
+            } else {
+                console.log(`Unknown project: ${trimmedProject}`);
+            }
+        }
     } else {
-        console.log('Starting all apps...');
-        const cmd = `cd .\\raaghu-mfe && npm run start`;
-        startCmd('All', cmd);
+        console.log('Starting Storybook by default...');
+        startCmd('Storybook', 'npm run storybook');
     }
-
 }
 
 start();
-
