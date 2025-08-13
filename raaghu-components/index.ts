d6c0b17b919ec0749412b83d708a8ce3639e277c
Nitin Sonawane <nitin.sonawane@waiin.com>
2025-08-13 15:23:00 +0530

    Commit Changes for task 2417 completed video player enhancement

--- raaghu-components/index.ts
+++ raaghu-components/index.ts
@@ -2,3 +2,4 @@ export {default as RdsCompSpinner} from './rds-comp-spinner/rds-comp-spinner';
 export {default as RdsCompToast} from './rds-comp-toast/rds-comp-toast';
 export {default as RdsCompScrollbar} from './rds-comp-scrollbar/rds-comp-scrollbar';
 export {default as RdsCompTruncatedText} from './rds-comp-truncate-text/rds-comp-truncate-text';
+export {default as RdsCompVideoPlayer} from './rds-comp-video-player/rds-comp-video-player';
