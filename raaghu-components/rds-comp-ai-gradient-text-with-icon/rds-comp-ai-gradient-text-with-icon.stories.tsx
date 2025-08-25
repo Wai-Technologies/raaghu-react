a932840bc603ecafb82be3b9962388dfbaee524e
AkshayPotphode <akshay.potphode@waiin.com>
2025-08-25 15:24:49 +0530

    Commit changes for gradient text with icon

---
+++ raaghu-components/rds-comp-ai-gradient-text-with-icon/rds-comp-ai-gradient-text-with-icon.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import RdsCompAiGradientTextWithIcon from "./rds-comp-ai-gradient-text-with-icon";
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+ 
+const meta: Meta = {
+    title: "Components/AI ChatBox/Gradient Text With Icon",
+    component: RdsCompAiGradientTextWithIcon,
+    argTypes: {
+       
+    },
+    parameters: {
+        layout: 'padded',
+        docs:{
+            description: {
+  component: `The **Gradient Text With Icon** component combines dynamic gradient-styled text with an accompanying icon or logo to create visually engaging headers or status indicators within the AI ChatBox interface. It accepts a \`logoUrl\` prop for specifying a remote image URL, and a \`logo\` prop to provide a local image source, allowing flexibility in icon usage. The \`title\` prop defines the primary text displayed alongside the icon, supporting customizable messaging. This component is ideal for highlighting processes, status updates, or branding elements, offering a modern and vibrant presentation that enhances user engagement through animated or static visuals paired with gradient typography.`
+}
+
+        }
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof RdsCompAiGradientTextWithIcon>;
+
+
+export default meta;
+type Story = StoryObj<typeof RdsCompAiGradientTextWithIcon>;
+
+export const Default: Story = {
+    args: {
+        logoUrl: "https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/pundit-color-logo.png",
+        logo: <AutoAwesomeIcon className="pundit-icon" />,
+        title: "AI Pundit is creating some magic for you",
+    },
+}
