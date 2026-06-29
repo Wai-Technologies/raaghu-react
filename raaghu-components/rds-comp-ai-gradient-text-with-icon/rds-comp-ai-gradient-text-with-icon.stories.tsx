4fc7d5367074f751d24426f4a7f8a049c82c5f96
Sagar-Bagwe <sagar.bagwe@waiin.com>
2026-06-29 10:43:26 +0530

    Remove ai pudit logo and add raaghu logo.

--- raaghu-components/rds-comp-ai-gradient-text-with-icon/rds-comp-ai-gradient-text-with-icon.stories.tsx
+++ raaghu-components/rds-comp-ai-gradient-text-with-icon/rds-comp-ai-gradient-text-with-icon.stories.tsx
@@ -19,10 +19,11 @@ type Story = StoryObj<typeof RdsCompAiGradientTextWithIcon>;
 
 export const Default: Story = {
     args: {
-        logoUrl: "https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/pundit-color-logo.png",
+        logoUrl: "https://raaghustorageaccount.blob.core.windows.net/raaghu-blob/raaghu_icon.png",
         logo: <AutoAwesomeIcon className="pundit-icon" />,
         title: "AI Pundit is creating some magic for you",
         showImage: true,
         showIcon: true,
-    },
+    },
+
 }
